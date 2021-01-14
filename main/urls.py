7986a40a7748c04855316320a7ba737a00518e6a
Matt Bertrand <mrbertrand@gmail.com>
2021-01-14 13:38:56 -0500

    pylint

--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from main.views import index
