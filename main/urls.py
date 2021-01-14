40833f315a1cf4acf654e378ea6537391e8bf4c1
George Schneeloch <gschneel@mit.edu>
2021-01-14 10:32:37 -0500

    Move to separate urls.py

--- main/urls.py
+++ main/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import path
 
 from main.views import index
-from news.views import news
 
 
 urlpatterns = [
@@ -28,7 +27,7 @@ urlpatterns = [
     path("robots.txt", include("robots.urls")),
     # Example view
     path("", index, name="main-index"),
-    path("api/news/", news, name="ocw-news"),
+    path("", include("news.urls")),
 ]
 
 if settings.DEBUG:
