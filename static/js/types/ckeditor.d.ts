e3b01e4a4cc9609810d338af0a5794dd7bc03508
Alice Pote <alice.writes.wrongs@gmail.com>
2021-02-04 10:29:44 -0500

    fix typescript issues

---
+++ static/js/types/ckeditor.d.ts
@@ -0,0 +1,35 @@
+declare module '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+declare module '@ckeditor/ckeditor5-list/src/list';
+
+declare module '@ckeditor/ckeditor5-link/src/link';
+
+declare module '@ckeditor/ckeditor5-image/src/imageupload';
+
+declare module '@ckeditor/ckeditor5-image/src/imageupload';
+
+declare module '@ckeditor/ckeditor5-image/src/imagetoolbar';
+
+declare module '@ckeditor/ckeditor5-image/src/imagestyle';
+
+declare module '@ckeditor/ckeditor5-image/src/imagecaption';
+
+declare module '@ckeditor/ckeditor5-image/src/image';
+
+declare module '@ckeditor/ckeditor5-heading/src/heading';
+
+declare module '@ckeditor/ckeditor5-easy-image/src/easyimage';
+
+declare module '@ckeditor/ckeditor5-block-quote/src/blockquote';
+
+declare module '@ckeditor/ckeditor5-basic-styles/src/italic';
+
+declare module '@ckeditor/ckeditor5-basic-styles/src/bold';
+
+declare module '@ckeditor/ckeditor5-autoformat/src/autoformat';
+
+declare module '@ckeditor/ckeditor5-essentials/src/essentials';
+
+declare module '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+declare module '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
