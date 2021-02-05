4971a7ac76d017a90218168e178527952e21e23f
Alice Pote <alice.writes.wrongs@gmail.com>
2021-02-05 12:44:18 -0500

    add in some test data to test initializing editor too

--- static/js/pages/MarkdownEditorTestPage.tsx
+++ static/js/pages/MarkdownEditorTestPage.tsx
@@ -2,13 +2,29 @@ import React, { useState } from "react"
 
 import MarkdownEditor from "../components/MarkdownEditor"
 
-const EDITOR_TAB = "editor"
-const MARKDOWN_TAB = "markdown"
+const TEST_DATA = `## A heading
+
+Amazing stuff! Paragraphs!
+
+And another paragraph!
+
+**bold** and even _italic_ text.
+
+*   a
+*   list
+*   of
+*   items
+*   including
+*   [links](https://reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node)
+
+also have some
+
+> block quotes
+
+good stuff.`
 
 export default function MarkdownEditorTestPage() {
-  const [markdown, setMarkdown] = useState("")
-
-  const [currentTab, setTab] = useState(EDITOR_TAB)
+  const [markdown, setMarkdown] = useState(TEST_DATA)
 
   return (
     <div>
@@ -18,11 +34,13 @@ export default function MarkdownEditorTestPage() {
       </div>
       <div className="w-50">
         <h3>Output</h3>
-        {markdown !== "" ? (
-          <pre style={{ border: "2px solid red" }}>
-            <code style={{ margin: "5px", display: "block" }}>{markdown}</code>
-          </pre>
-        ) : null}
+    { markdown !== "" ?
+        <pre style={{border: "2px solid red" }}>
+          <code style={{margin: "5px", display: "block"}}>
+            { markdown }
+          </code>
+        </pre>
+        : null }
       </div>
     </div>
   )
