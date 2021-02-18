ac37530b1fa15cc16eb6cab03d42ea8487b7f6c8
Alice Pote <alice.writes.wrongs@gmail.com>
2021-02-18 09:47:54 -0500

    remove youtube embed code from the md <-> html library

--- static/js/lib/markdown.ts
+++ static/js/lib/markdown.ts
@@ -2,57 +2,11 @@ import { Converter } from "showdown"
 import TurndownService from "turndown"
 import { turndownService } from "@ckeditor/ckeditor5-markdown-gfm/src/html2markdown/html2markdown"
 
-export const YOUTUBE_SHORTCODE_REGEX = /{{< youtube "(\S+)" >}}/
-export const YOUTUBE_SRC_REGEX = /https:\/\/www\.youtube\.com\/embed\/(\S+)\/?$/
-export const YOUTUBE_EMBED_CLASS = "youtube-embed"
-
-export const youtubeEmbedUrl = (videoId: string): string =>
-  `https://www.youtube.com/embed/${videoId}`
-
-export const YOUTUBE_EMBED_PARAMS = {
-  width:       "560",
-  height:      "315",
-  frameborder: "0",
-  allow:
-    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
-  allowfullscreen: true
-}
-
-function youtubeShortcodeExtension() {
-  return [
-    {
-      type:    "lang",
-      regex:   YOUTUBE_SHORTCODE_REGEX,
-      replace: (s: string, match: string) =>
-        `<section class="${YOUTUBE_EMBED_CLASS}">${match}</section>`
-    }
-  ]
-}
-
-turndownService.addRule("youtubeEmbed", {
-  filter: (node: HTMLElement, _: any) => {
-    return (
-      node.nodeName === "SECTION" &&
-      node.getAttribute("class") === YOUTUBE_EMBED_CLASS
-    )
-  },
-  replacement: (
-    content: string,
-    node: TurndownService.Node,
-    options: TurndownService.Options
-  ): string => {
-    const videoId = content.replace(/\\/, "")
-    return `{{< youtube "${videoId}" >}}`
-  }
-})
-
 export function html2md(html: string): string {
   return turndownService.turndown(html)
 }
 
-const converter = new Converter({
-  extensions: [youtubeShortcodeExtension]
-})
+const converter = new Converter()
 
 export function md2html(md: string): string {
   return converter.makeHtml(md)
