bfc7a912e3adac67b4bd5efcfbca10b43e067902
Alice Pote <alice.writes.wrongs@gmail.com>
2021-02-19 15:36:12 -0500

    argh this is so frustrating

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,20 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>static/js/test_setup.ts"],
   transform: {
-    "^.+\\.[tj]sx?$": "ts-jest",
+    "^.+\\.[tj]sx?$": "ts-jest"
   },
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/static/js/mocks/fileMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/static/js/mocks/fileMock.js",
     "\\.(css|less)$": "<rootDir>/static/js/mocks/styleMock.js"
   },
-  transformIgnorePatterns: ['/node_modules/(?!@ckeditor|html2markdown|ckeditor|ckeditor5-editor-classic|@ckeditor/*)/.+\\.js$'],
+  transformIgnorePatterns: [
+    "/node_modules/(?!" +
+      "@ckeditor/ckeditor/ckeditor5-editor-classic" +
+      "|@ckeditor/ckeditor/ckeditor5-editor-classic/src/classiceditor" +
+      "|ckeditor" +
+      ")/.+\\.js$"
+  ],
+  // |html2markdown|ckeditor|ckeditor5-editor-classic|@ckeditor/*)/.+\\.js$'],
   testPathIgnorePatterns: ["<rootDir>/staticfiles/", "<rootDir>/node_modules/"]
 }
