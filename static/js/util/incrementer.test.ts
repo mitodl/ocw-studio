ff97855bf50d12fa36d997c4eec757c2374dee89
George Schneeloch <gschneel@mit.edu>
2021-02-19 14:25:08 -0500

    Update static/js/util/incrementer.test.ts
    Co-authored-by: Gavin Sidebottom <gwsidebottom@gmail.com>

--- static/js/util/incrementer.test.ts
+++ static/js/util/incrementer.test.ts
@@ -1,6 +1,6 @@
 import incrementer from "./incrementer"
 
-describe("incrementor", () => {
+describe("incrementer", () => {
   it("increments", () => {
     const incr = incrementer()
     for (let counter = 1; counter < 10; ++counter) {
