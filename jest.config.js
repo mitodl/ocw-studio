7e29d72b96430fa468784561b94d824cb6deed77
Alice Pote <alice.writes.wrongs@gmail.com>
2021-02-19 16:01:44 -0500

    well that sort of works

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>static/js/test_setup.ts"],
   transform: {
-    "^.+\\.[tj]sx?$": "ts-jest"
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
   },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
@@ -9,12 +10,15 @@ module.exports = {
     "\\.(css|less)$": "<rootDir>/static/js/mocks/styleMock.js"
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!" +
-      "@ckeditor/ckeditor/ckeditor5-editor-classic" +
-      "|@ckeditor/ckeditor/ckeditor5-editor-classic/src/classiceditor" +
-      "|ckeditor" +
-      ")/.+\\.js$"
+    "/node_modules/(?!(" +
+      "@ckeditor/ckeditor5-editor-classic" +
+      "|@ckeditor/ckeditor5-engine" +
+      "|@ckeditor/ckeditor5-core" +
+      "|@ckeditor/ckeditor5-markdown-gfm" +
+      "|@ckeditor/ckeditor5-utils" +
+      "|@ckeditor/ckeditor5-ui" +
+      "|lodash-es" +
+      ")/)"
   ],
-  // |html2markdown|ckeditor|ckeditor5-editor-classic|@ckeditor/*)/.+\\.js$'],
   testPathIgnorePatterns: ["<rootDir>/staticfiles/", "<rootDir>/node_modules/"]
 }
