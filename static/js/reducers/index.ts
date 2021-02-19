b1980dc73918155b9581ae057b22f48b74eccbb3
George Schneeloch <gschneel@mit.edu>
2021-02-19 14:01:37 -0500

    Set ReduxState from type of reducer

--- static/js/reducers/index.ts
+++ static/js/reducers/index.ts
@@ -1,13 +1,10 @@
-import { QueriesState, Entities } from "redux-query"
 import { combineReducers } from "redux"
 import { entitiesReducer, queriesReducer } from "redux-query"
 
-export interface ReduxState {
-  queries: QueriesState
-  entities: Entities
-}
-
-export default combineReducers({
+const rootReducer = combineReducers({
   entities: entitiesReducer,
   queries:  queriesReducer
 })
+export default rootReducer
+
+export type ReduxState = ReturnType<typeof rootReducer>
