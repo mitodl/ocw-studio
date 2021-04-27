a9a50eec5a3594ea8380b07735fceb26c80e0159
Nathan Levesque <rhysyngsun@gmail.com>
2021-04-27 12:54:03 -0400

    Hide the login button if the user is logged in

--- static/js/pages/HomePage.tsx
+++ static/js/pages/HomePage.tsx
@@ -3,9 +3,11 @@ import React from "react"
 export default function HomePage(): JSX.Element | null {
   return (
     <div className="d-flex align-items-center justify-content-center home-page">
-      <a href="/login/saml/?idp=default" className="btn green-button">
-        Login with MIT Touchstone
-      </a>
+      {!SETTINGS.user ? (
+        <a href="/login/saml/?idp=default" className="btn green-button">
+          Login with MIT Touchstone
+        </a>
+      ) : null}
     </div>
   )
 }
