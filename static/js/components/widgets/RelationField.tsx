39cf2f4bad4e6f1e9407376c6f005e8f128dab22
Alice Pote <alice.writes.wrongs@gmail.com>
2021-05-05 13:33:19 -0400

    gah these types are a huge mess

--- static/js/components/widgets/RelationField.tsx
+++ static/js/components/widgets/RelationField.tsx
@@ -1,31 +1,40 @@
-import React, { useState, useContext, useEffect } from "react"
+import React, { useState, useContext, useEffect, useMemo } from "react"
+import Select from "react-select"
 import { useRequest } from "redux-query-react"
 import { useSelector } from "react-redux"
 
+import SelectField from './SelectField'
 import WebsiteContext from "../../context/Website"
+
 import { websiteContentListingRequest } from "../../query-configs/websites"
 import { WEBSITE_CONTENT_PAGE_SIZE } from "../../constants"
 import { getWebsiteContentListingCursor } from "../../selectors/websites"
 
-import { ContentListingParams } from "../../types/websites"
+import { Option } from './SelectField'
 
 interface Props {
+  name: string
   collection: string
   display_field: string
   max: number
   min: number
   multiple: boolean
-  search_fields: string[]
+  onChange: (event: Event) => void
+  value: any
 }
 
 export default function RelationField(props: Props): JSX.Element {
-  const { collection, display_field, max, min, multiple, search_fields } = props
+  const { collection, display_field, name,  multiple, onChange, value } = props
+
+  console.log(props);
 
   const [offset, setOffset] = useState(0)
 
-  const { name } = useContext(WebsiteContext) ?? {}
+  // technically the Website in WebsiteContext can be null, but in the
+  // content where this component is mounted it never should be in practice.
+  const website = useContext(WebsiteContext)
 
-  const listingParams = name ? { name, type: collection, offset } : null
+  const listingParams = website ? { name: website.name, type: collection, offset } : null
 
   useRequest(listingParams ? websiteContentListingRequest(listingParams) : null)
 
@@ -39,16 +48,38 @@ export default function RelationField(props: Props): JSX.Element {
   const count = listing?.count ?? 0
 
   useEffect(() => {
-    console.log('effecting');
-    console.log(count);
-    console.log(offset);
-
-
-    if (count - (offset + WEBSITE_CONTENT_PAGE_SIZE) > 0) {
-      console.log('bumping offset');
+    // check if we need to re-run the request
+    //
+    // if count is greater than our current offset plus page size (i.e. the
+    // number of items which will be fetched in the current request) then we
+    // need to bump up offset by WEBSITE_CONTENT_PAGE_SIZE to fetch the next
+    // page.
+    //
+    // this will then change the listingParams object and fire off a new
+    // request.
+    if (count > offset + WEBSITE_CONTENT_PAGE_SIZE) {
       setOffset(offset => (offset += WEBSITE_CONTENT_PAGE_SIZE))
     }
   }, [offset, setOffset, count])
 
-  return <div>hey!</div>
+  console.log(listing);
+
+  const options: Option[] = useMemo(() => {
+    const options = (listing?.results ?? []).map((entry: any) => ({
+      label: entry[display_field],
+      value: entry.text_id
+    }))
+
+    return options
+  }, [listing])
+
+
+  return <SelectField
+    name={name}
+    value={value}
+    onChange={onChange}
+    options={options}
+    multiple={multiple}
+  />
 }
+
