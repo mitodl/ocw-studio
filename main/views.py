bdcaa78dd552d01dd6e929d0a3085df973b1de61
George Schneeloch <gschneel@mit.edu>
2021-06-04 09:02:20 -0400

    Restrict routes not used to login or view home page (#299)

--- main/views.py
+++ main/views.py
@@ -4,15 +4,14 @@ ocw_studio views
 import json
 
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from mitol.common.utils.webpack import webpack_public_path
 from rest_framework.pagination import LimitOffsetPagination
 
 
-def index(request, *args, **kwargs):  # pylint: disable=unused-argument
-    """
-    The index view. Display available programs
-    """
+def _index(request):
+    """Render the view for React pages"""
 
     js_settings = {
         "gaTrackingID": settings.GA_TRACKING_ID,
@@ -41,6 +40,21 @@ def index(request, *args, **kwargs):  # pylint: disable=unused-argument
             "js_settings_json": json.dumps(js_settings),
         },
     )
+
+
+def public_index(request, *args, **kwargs):  # pylint: disable=unused-argument
+    """
+    Render a React page. The frontend will render an appropriate UI based on the URL
+    """
+    return _index(request)
+
+
+@login_required
+def restricted_index(request, *args, **kwargs):  # pylint: disable=unused-argument
+    """
+    Render a React page. The frontend will render an appropriate UI based on the URL
+    """
+    return _index(request)
 
 
 class DefaultPagination(LimitOffsetPagination):
