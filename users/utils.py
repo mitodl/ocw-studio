acd0fba8220bfecf02e348b49ba120a8e97ffbac
Matt Bertrand <mrbertrand@gmail.com>
2021-08-11 10:50:27 -0400

    lint

--- users/utils.py
+++ users/utils.py
@@ -4,7 +4,7 @@ from email.utils import formataddr
 from users.models import User
 
 
-def format_recipient(user: User):
+def format_recipient(user: User) -> str:
     """
     Format a user as a recipient for an email
     """
--- users/utils_test.py
+++ users/utils_test.py
@@ -4,5 +4,6 @@ from users.utils import format_recipient
 
 
 def test_format_recipient():
+    """format_recipient should return the expected string"""
     user = UserFactory.build()
     assert format_recipient(user) == f'"{user.email}" <{user.email}>'
