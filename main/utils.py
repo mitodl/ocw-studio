cf5338ec7d4dec2e62aefcb2ac5efbb34e820971
Carey Gumaer <gumaerc@mit.edu>
2021-08-19 10:53:35 -0400

    make UUID check version agnostic by default and add tests

--- main/utils.py
+++ main/utils.py
@@ -31,12 +31,15 @@ def uuid_string():
     return str(uuid4())
 
 
-def is_valid_uuid(uuid_to_test: str, version: int = 4) -> bool:
+def is_valid_uuid(uuid_to_test: str, version: None) -> bool:
     """
     Returns True if the given string is a valid UUID
     """
     try:
-        uuid_obj = UUID(uuid_to_test, version=version)
+        if version:
+            uuid_obj = UUID(uuid_to_test, version=version)
+        else:
+            uuid_obj = UUID(uuid_to_test)
     except ValueError:
         return False
     return str(uuid_obj) == uuid_to_test
