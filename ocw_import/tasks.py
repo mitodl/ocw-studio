eb42f2f440fc7a8e1d5d91df507a7613d3eced55
Carey P Gumaer <gumaerc@mit.edu>
2021-09-03 16:17:39 -0400

    delete unpublished courses take two (#551)
    * wip
    
    * separate delete_unpublished_courses into its own task and always base it on all courses found in the S3 bucet
    
    * change a bit of the import test infrastructure and add a test for deleting unpublished courses
    
    * directly test delete_unpublished_courses task, clean up formatting stuff
    
    * remove unnecessary setting, add test for when delete_unpublished is set to False
    
    * remove unnecessary variable declaration

--- ocw_import/tasks.py
+++ ocw_import/tasks.py
@@ -7,7 +7,8 @@ from mitol.common.utils.collections import chunks
 
 from main.celery import app
 from ocw_import.api import fetch_ocw2hugo_course_paths, import_ocw2hugo_course
-from websites.models import WebsiteStarter
+from websites.constants import WEBSITE_SOURCE_OCW_IMPORT
+from websites.models import Website, WebsiteStarter
 
 
 log = logging.getLogger(__name__)
@@ -19,10 +20,9 @@ def import_ocw2hugo_course_paths(paths=None, bucket_name=None, prefix=None):
     Import all ocw2hugo courses & content
 
     Args:
-        paths (list of str): list of course url paths
+        paths (list of str): list of course data template paths
         bucket_name (str): S3 bucket name
         prefix (str): S3 prefix before start of course_id path
-
     """
     if not paths:
         return
@@ -38,9 +38,37 @@ def import_ocw2hugo_course_paths(paths=None, bucket_name=None, prefix=None):
         )
 
 
+@app.task()
+def delete_unpublished_courses(paths=None):
+    """
+    Delete all unpublished courses based on paths not present for existing Websites
+
+    Args:
+        paths: (list of str): list of course data template paths
+    """
+    if not paths:
+        return
+    course_ids = list(map((lambda key: key.replace("/data/course.json", "", 1)), paths))
+    unpublished_courses = Website.objects.filter(
+        source=WEBSITE_SOURCE_OCW_IMPORT
+    ).exclude(name__in=course_ids)
+    if unpublished_courses.count() == 0:
+        log.info("No unpublished courses to delete")
+        return
+    else:
+        log.info("Deleting unpublished courses: %s", unpublished_courses)
+        unpublished_courses.delete()
+
+
 @app.task(bind=True)
 def import_ocw2hugo_courses(
-    self, bucket_name=None, prefix=None, filter_str=None, limit=None, chunk_size=100
+    self,
+    bucket_name=None,
+    prefix=None,
+    filter_str=None,
+    limit=None,
+    delete_unpublished=True,
+    chunk_size=100,
 ):  # pylint:disable=too-many-arguments
     """
     Import all ocw2hugo courses & content
@@ -50,21 +78,30 @@ def import_ocw2hugo_courses(
         prefix (str): (Optional) S3 prefix before start of course_id path
         filter_str (str): (Optional) If specified, only yield course paths containing this string
         limit (int or None): (Optional) If specified, limits the number of courses imported
+        delete_unpublished (bool): (Optional) If true, delete unpublished courses from the DB
         chunk_size (int): Number of courses to process per task
     """
     if not bucket_name:
         raise TypeError("Bucket name must be specified")
-    course_paths = iter(
-        fetch_ocw2hugo_course_paths(bucket_name, prefix=prefix, filter_str=filter_str)
-    )
+    course_paths = list(fetch_ocw2hugo_course_paths(bucket_name, prefix=prefix))
+    if delete_unpublished:
+        delete_unpublished_courses_task = delete_unpublished_courses.si(
+            paths=course_paths
+        )
+    else:
+        delete_unpublished_courses_task = None
+    if filter_str is not None:
+        course_paths = [path for path in course_paths if filter_str in path]
     if limit is not None:
-        course_paths = (path for i, path in enumerate(course_paths) if i < limit)
-    course_tasks = celery.group(
-        [
-            import_ocw2hugo_course_paths.si(
-                paths=paths, bucket_name=bucket_name, prefix=prefix
-            )
-            for paths in chunks(course_paths, chunk_size=chunk_size)
-        ]
-    )
-    raise self.replace(course_tasks)
+        course_paths = course_paths[:limit]
+    course_tasks = [
+        import_ocw2hugo_course_paths.si(
+            paths=paths,
+            bucket_name=bucket_name,
+            prefix=prefix,
+        )
+        for paths in chunks(course_paths, chunk_size=chunk_size)
+    ]
+    if delete_unpublished_courses_task is not None:
+        course_tasks.append(delete_unpublished_courses_task)
+    raise self.replace(celery.group(course_tasks))
--- ocw_import/tasks_test.py
+++ ocw_import/tasks_test.py
@@ -1,13 +1,33 @@
 """ Tests for ocw_import.tasks """
+from tempfile import TemporaryDirectory
+
 import pytest
 from moto import mock_s3
 
-from ocw_import.conftest import MOCK_BUCKET_NAME, TEST_OCW2HUGO_PREFIX, setup_s3
-from ocw_import.tasks import import_ocw2hugo_course_paths, import_ocw2hugo_courses
+from ocw_import.api import import_ocw2hugo_course
+from ocw_import.conftest import (
+    MOCK_BUCKET_NAME,
+    TEST_OCW2HUGO_PREFIX,
+    setup_s3,
+    setup_s3_tmpdir,
+)
+from ocw_import.tasks import (
+    delete_unpublished_courses,
+    import_ocw2hugo_course_paths,
+    import_ocw2hugo_courses,
+)
+from websites.models import Website
 
 
 # pylint:disable=too-many-arguments
 pytestmark = pytest.mark.django_db
+
+ALL_COURSES_PATHS = [
+    "1-050-engineering-mechanics-i-fall-2007/data/course.json",
+    "1-201j-transportation-systems-analysis-demand-and-economics-fall-2008/data/course.json",
+    "biology/data/course.json",
+]
+SINGLE_COURSE_PATHS = ["1-050-engineering-mechanics-i-fall-2007/data/course.json"]
 
 
 @pytest.mark.parametrize(
@@ -62,3 +82,66 @@ def test_import_ocw2hugo_courses_nobucket(mocker):
             bucket_name=None, prefix=TEST_OCW2HUGO_PREFIX, chunk_size=100
         )
     assert mock_import_paths.call_count == 0
+
+
+@mock_s3
+def test_delete_unpublished_courses(settings, course_starter):
+    """ delete_unpublished_courses should remove Website objects when they aren't present in the passed in list of paths """
+    setup_s3(settings)
+    settings.OCW_IMPORT_STARTER_SLUG = "course"
+    for course_path in ALL_COURSES_PATHS:
+        import_ocw2hugo_course(
+            MOCK_BUCKET_NAME,
+            TEST_OCW2HUGO_PREFIX,
+            course_path,
+            starter_id=course_starter.id,
+        )
+    assert Website.objects.all().count() == 3
+    delete_unpublished_courses(paths=ALL_COURSES_PATHS)
+    assert Website.objects.all().count() == 3
+    delete_unpublished_courses(paths=SINGLE_COURSE_PATHS)
+    assert Website.objects.all().count() == 2
+
+
+@mock_s3
+def test_import_ocw2hugo_courses_delete_unpublished(settings, mocker, mocked_celery):
+    """ import_ocw2hugo_courses should call delete_unpublished when courses have been removed from the ocw-to-hugo directory """
+    mock_delete_unpublished_courses = mocker.patch(
+        "ocw_import.tasks.delete_unpublished_courses.si"
+    )
+    tmpdir = TemporaryDirectory()
+    setup_s3_tmpdir(settings, tmpdir.name)
+    with pytest.raises(mocked_celery.replace_exception_class):
+        import_ocw2hugo_courses.delay(
+            bucket_name=MOCK_BUCKET_NAME, prefix=TEST_OCW2HUGO_PREFIX
+        )
+    mock_delete_unpublished_courses.assert_called_with(paths=ALL_COURSES_PATHS)
+    tmpdir.cleanup()
+    tmpdir = TemporaryDirectory()
+    setup_s3_tmpdir(
+        settings, tmpdir.name, courses=["1-050-engineering-mechanics-i-fall-2007"]
+    )
+    with pytest.raises(mocked_celery.replace_exception_class):
+        import_ocw2hugo_courses.delay(
+            bucket_name=MOCK_BUCKET_NAME, prefix=TEST_OCW2HUGO_PREFIX
+        )
+    mock_delete_unpublished_courses.assert_called_with(paths=SINGLE_COURSE_PATHS)
+    tmpdir.cleanup()
+
+
+@mock_s3
+def test_import_ocw2hugo_courses_delete_unpublished_false(
+    settings, mocker, mocked_celery
+):
+    """ import_ocw2hugo_courses should not call delete_unpublished when the argument is false """
+    mock_delete_unpublished_courses = mocker.patch(
+        "ocw_import.tasks.delete_unpublished_courses.si"
+    )
+    setup_s3(settings)
+    with pytest.raises(mocked_celery.replace_exception_class):
+        import_ocw2hugo_courses.delay(
+            bucket_name=MOCK_BUCKET_NAME,
+            prefix=TEST_OCW2HUGO_PREFIX,
+            delete_unpublished=False,
+        )
+    mock_delete_unpublished_courses.assert_not_called()
