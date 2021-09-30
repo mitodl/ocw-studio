88cd7cb3fbef8cea072f1088d842867a86e0e39e
George Schneeloch <gschneel@mit.edu>
2021-09-30 16:25:41 -0400

    Trim content from destination url (#627)

--- content_sync/utils_test.py
+++ content_sync/utils_test.py
@@ -1,9 +1,12 @@
 """Content sync utility functionality tests"""
 import pytest
 
-from content_sync.utils import get_destination_filepath
+from content_sync.utils import get_destination_filepath, get_destination_url
 from websites.factories import WebsiteContentFactory, WebsiteStarterFactory
 from websites.site_config_api import ConfigItem, SiteConfig
+
+
+pytestmark = pytest.mark.django_db
 
 
 @pytest.mark.parametrize(
@@ -54,8 +57,61 @@ def test_get_destination_url_errors(mocker):
         is_page_content=False,
         type=config_item_name,
     )
-    return_value = get_destination_filepath(
+    return_value = get_destination_url(
         content=content, site_config=SiteConfig(starter.config)
     )
     patched_log.error.assert_called_once()
     assert return_value is None
+
+
+@pytest.mark.parametrize(
+    "is_page_content, dirpath, filename, expected",
+    [
+        [True, "content/pages", "_index", "pages/"],
+        [True, "content/pages", "hx_network", "pages/hx_network"],
+        [
+            True,
+            "content/pages/lecture-notes",
+            "java_3d_lecture",
+            "pages/lecture-notes/java_3d_lecture",
+        ],
+        [True, "content/resources", "image", "resources/image"],
+        [False, "", "", None],
+    ],
+)
+def test_get_destination_url(is_page_content, dirpath, filename, expected):
+    """get_destination_url should create a url for a piece of content"""
+    content = WebsiteContentFactory.create(
+        is_page_content=is_page_content, dirpath=dirpath, filename=filename
+    )
+    assert (
+        get_destination_url(content, SiteConfig(content.website.starter.config))
+        == expected
+    )
+
+
+@pytest.mark.parametrize(
+    "is_page_content, dirpath, filename, expected",
+    [
+        [True, "content/pages", "_index", "content/pages/_index.md"],
+        [True, "content/pages", "hx_network", "content/pages/hx_network.md"],
+        [
+            True,
+            "content/pages/lecture-notes",
+            "java_3d_lecture",
+            "content/pages/lecture-notes/java_3d_lecture.md",
+        ],
+        [True, "content/resources", "image", "content/resources/image.md"],
+        [False, "", "", None],
+        [False, "config/_default/menus.yaml", "menus.yaml", None],
+    ],
+)
+def test_get_destination_filepath(is_page_content, dirpath, filename, expected):
+    """get_destination_filepath should create the filepath for a piece of content"""
+    content = WebsiteContentFactory.create(
+        is_page_content=is_page_content, dirpath=dirpath, filename=filename
+    )
+    assert (
+        get_destination_filepath(content, SiteConfig(content.website.starter.config))
+        == expected
+    )
