48a9c065b9ee6e8fcd134ad1f4bdb2906088cd96
Gavin Sidebottom <gsidebo@mit.edu>
2021-10-12 14:42:05 -0500

    Added redesigned site header

--- static/js/pages/App.test.tsx
+++ static/js/pages/App.test.tsx
@@ -1,14 +1,27 @@
+import sinon from "sinon"
+
 import App from "./App"
 import IntegrationTestHelper, {
   TestRenderer
 } from "../util/integration_test_helper"
+import { makeWebsiteDetail } from "../util/factories/websites"
+import { siteApiDetailUrl, sitesBaseUrl } from "../lib/urls"
+
+import { Website } from "../types/websites"
 
 describe("App", () => {
-  let helper: IntegrationTestHelper, render: TestRenderer
+  let helper: IntegrationTestHelper,
+    render: TestRenderer,
+    website: Website,
+    siteDetailApiUrl: string
 
   beforeEach(() => {
     helper = new IntegrationTestHelper()
     render = helper.configureRenderer(App)
+
+    website = makeWebsiteDetail()
+    siteDetailApiUrl = siteApiDetailUrl.param({ name: website.name }).toString()
+    helper.mockGetRequest(siteDetailApiUrl, website)
   })
 
   afterEach(() => {
@@ -25,5 +38,21 @@ describe("App", () => {
     const app = wrapper.find("App")
     const header = app.find("Header")
     expect(header.exists()).toBeTruthy()
+  })
+
+  it("should not make a request for website detail", async () => {
+    await render()
+    sinon.assert.notCalled(helper.handleRequestStub)
+  })
+
+  describe("when on a website detail URL", () => {
+    it("should load website from the API and render the SitePage component", async () => {
+      const siteDetailUrl = `${sitesBaseUrl.toString()}${website.name}`
+      helper.browserHistory.push(siteDetailUrl)
+      const { wrapper } = await render()
+      sinon.assert.calledWith(helper.handleRequestStub, siteDetailApiUrl, "GET")
+      const sitePageComponent = wrapper.find("SitePage")
+      expect(sitePageComponent.exists()).toBe(true)
+    })
   })
 })
--- static/js/pages/SitePage.test.tsx
+++ static/js/pages/SitePage.test.tsx
@@ -1,74 +1,64 @@
-const mockUseRouteMatch = jest.fn()
-
-import { act } from "react-dom/test-utils"
-
-import SitePage from "./SitePage"
+import * as React from "react"
 
 import IntegrationTestHelper, {
   TestRenderer
 } from "../util/integration_test_helper"
 import { makeWebsiteDetail } from "../util/factories/websites"
-import { siteApiDetailUrl } from "../lib/urls"
+import { siteApiCollaboratorsUrl } from "../lib/urls"
+import SitePage from "./SitePage"
+import WebsiteContext from "../context/Website"
+
 import { Website } from "../types/websites"
 
-jest.mock("react-router-dom", () => ({
-  // @ts-ignore
-  ...jest.requireActual("react-router-dom"),
-  useRouteMatch: mockUseRouteMatch
-}))
-
 describe("SitePage", () => {
+  const siteName = "fakeSiteName"
   let helper: IntegrationTestHelper, render: TestRenderer, website: Website
 
   beforeEach(() => {
     helper = new IntegrationTestHelper()
-    website = makeWebsiteDetail()
-    render = helper.configureRenderer(
-      SitePage,
-      {},
-      {
-        entities: {
-          websiteDetails: {}
-        },
-        queries: {}
-      }
-    )
-    mockUseRouteMatch.mockImplementation(() => ({
-      params: {
-        name: website.name
-      }
-    }))
-
-    helper.mockGetRequest(
-      siteApiDetailUrl.param({ name: website.name }).toString(),
-      website
-    )
+    website = {
+      ...makeWebsiteDetail(),
+      name: siteName
+    }
+    helper.handleRequestStub
+      .withArgs(
+        siteApiCollaboratorsUrl.param({ name: website.name }).toString(),
+        "GET"
+      )
+      .returns({
+        body:   { results: [] },
+        status: 200
+      })
+    render = helper.configureRenderer(props => (
+      <WebsiteContext.Provider value={website}>
+        <SitePage {...props} />
+      </WebsiteContext.Provider>
+    ))
   })
 
   afterEach(() => {
     helper.cleanup()
   })
 
+  it("renders a loading message", async () => {
+    const { wrapper } = await render({ isLoading: true })
+    expect(wrapper.text()).toEqual("Loading...")
+  })
+
   it("renders the sidebar", async () => {
     const { wrapper } = await render()
     expect(wrapper.find("SiteSidebar").prop("website")).toBe(website)
-    expect(wrapper.find("h1.title").text()).toBe(website.title)
   })
 
-  it("toggles the publish drawer", async () => {
-    const { wrapper } = await render()
-    expect(wrapper.find("PublishDrawer").prop("visibility")).toBeFalsy()
-    act(() => {
-      // @ts-ignore
-      wrapper.find("PublishDrawer").prop("toggleVisibility")()
+  //
+  ;[
+    [`/sites/${siteName}/collaborators/`, "SiteCollaboratorList"],
+    [`/sites/${siteName}/type/some-type/`, "SiteContentListing"]
+  ].forEach(([url, expComponent]) => {
+    it(`renders a ${expComponent} component when the browser URL matches`, async () => {
+      helper.browserHistory.push(url)
+      const { wrapper } = await render()
+      expect(wrapper.find(expComponent).exists()).toBe(true)
     })
-    wrapper.update()
-    expect(wrapper.find("PublishDrawer").prop("visibility")).toBeTruthy()
-    act(() => {
-      // @ts-ignore
-      wrapper.find("PublishDrawer").prop("toggleVisibility")()
-    })
-    wrapper.update()
-    expect(wrapper.find("PublishDrawer").prop("visibility")).toBeFalsy()
   })
 })
