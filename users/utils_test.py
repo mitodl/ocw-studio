98f8e303ca615aca58b5b35f62a78bf332aed0c7
Matt Bertrand <mattbert@mit.edu>
2021-10-18 11:49:16 -0400

    Fix flaky test (test_format_recipient) (#713)

--- users/utils_test.py
+++ users/utils_test.py
@@ -1,9 +1,28 @@
 """Tests for user utils"""
+import pytest
+
 from users.factories import UserFactory
 from users.utils import format_recipient
 
 
-def test_format_recipient():
+@pytest.mark.parametrize(
+    "name,email,expected",
+    [
+        [
+            "Mr. John Thornton",
+            "jthornton@test.edu",
+            '"Mr. John Thornton" <jthornton@test.edu>',
+        ],
+        ["John Thornton", "jthornton@test.com", "John Thornton <jthornton@test.com>"],
+        ["Joanne O'Brien", "jobrien@test.edu", "Joanne O'Brien <jobrien@test.edu>"],
+        [
+            "Cpl. Joanne O'Brien",
+            "jobrien@test.edu",
+            '"Cpl. Joanne O\'Brien" <jobrien@test.edu>',
+        ],
+    ],
+)
+def test_format_recipient(name, email, expected):
     """format_recipient should return the expected string"""
-    user = UserFactory.build()
-    assert format_recipient(user) == f"{user.name} <{user.email}>"
+    user = UserFactory.build(name=name, email=email)
+    assert format_recipient(user) == expected
