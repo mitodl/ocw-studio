5daf2fea1a280eb60e5c94cdc1d1cef411db158c
Carey Gumaer <gumaerc@mit.edu>
2022-01-31 17:00:03 -0500

    more linting

--- content_sync/pipelines/base.py
+++ content_sync/pipelines/base.py
@@ -2,38 +2,18 @@
 import abc
 from typing import Optional
 
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
-
 from content_sync.constants import VERSION_DRAFT, VERSION_LIVE
+from content_sync.utils import check_mandatory_settings
 from websites.models import Website
 
 
 class BasePipeline(abc.ABC):
     """ Base class for a pipeline """
 
-    def __init__(self, api: Optional[object] = None):
-        """Make sure all required settings are present"""
-        self.api = api or self.__class__.get_api()
-
     @staticmethod
     @abc.abstractmethod
     def get_api():
         """Get a pipeline API instance"""
-        ...
-
-    @abc.abstractmethod
-    def trigger_pipeline_build(self, version: str) -> int:
-        """
-        Called to trigger the website pipeline.
-        """
-        ...
-
-    @abc.abstractmethod
-    def unpause_pipeline(self, version: str):
-        """
-        Called to unpause a website pipeline.
-        """
         ...
 
     @abc.abstractmethod
@@ -55,22 +35,10 @@ class BaseSyncPipeline(BasePipeline):
     VERSION_DRAFT = VERSION_DRAFT
 
     def __init__(self, website: Website, api: Optional[object] = None):
-        """Make sure all required settings are present"""
-        missing_settings = []
-        for setting_name in self.MANDATORY_SETTINGS:
-            if getattr(settings, setting_name, None) in (
-                None,
-                "",
-            ):
-                missing_settings.append(setting_name)
-        if missing_settings:
-            raise ImproperlyConfigured(
-                "The following settings are missing: {}".format(
-                    ", ".join(missing_settings)
-                )
-            )
-        self.website = website
+        if self.MANDATORY_SETTINGS:
+            check_mandatory_settings(self.MANDATORY_SETTINGS)
         self.api = api or self.__class__.get_api()
+        self.website = website
 
     @abc.abstractmethod
     def upsert_website_pipeline(self):  # pragma: no cover
@@ -79,9 +47,28 @@ class BaseSyncPipeline(BasePipeline):
         """
         ...
 
+    @abc.abstractmethod
+    def trigger_pipeline_build(self, version: str) -> int:
+        """
+        Called to trigger the website pipeline.
+        """
+        ...
+
+    @abc.abstractmethod
+    def unpause_pipeline(self, version: str):
+        """
+        Called to unpause a website pipeline.
+        """
+        ...
+
 
 class BaseThemeAssetsPipeline(BasePipeline):
-    """ Base class for preview/publish pipelines """
+    """ Base class for theme assets pipeline """
+
+    def __init__(self, api: Optional[object] = None):
+        if self.MANDATORY_SETTINGS:
+            check_mandatory_settings(self.MANDATORY_SETTINGS)
+        self.api = api or self.__class__.get_api()
 
     @abc.abstractmethod
     def trigger_pipeline_build(self) -> int:
