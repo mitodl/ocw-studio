99ac241b518082f0f40c2499813961b475b8ac47
Carey Gumaer <gumaerc@mit.edu>
2022-03-01 10:02:46 -0500

    capture all args as one capture group

--- websites/management/commands/markdown_cleaning/resource_link_delimiters.py
+++ websites/management/commands/markdown_cleaning/resource_link_delimiters.py
@@ -16,11 +16,10 @@ from websites.models import WebsiteContent
 class ResourceLinkDelimitersReplacementRule(MarkdownCleanupRule):
     """Replacement rule for use with WebsiteContentMarkdownCleaner."""
 
-    regex = r"{{<\sresource_link\s(?P<resource_uuid>[a-z0-9\-]*)\s(?P<link_text>\".*?\")\s>}}"
+    regex = r"{{<\sresource_link\s(?P<args>.*?)\s>}}"
 
     alias = "resource_link_delimiter_swap"
 
     def __call__(self, match: re.Match, website_content: WebsiteContent):
-        escaped_title = match.group("resource_uuid")
-        link_text = match.group("link_text")
-        return f"{{{{% resource_link {escaped_title} {link_text} %}}}}"
+        args = match.group("args")
+        return f"{{{{% resource_link {args} %}}}}"
