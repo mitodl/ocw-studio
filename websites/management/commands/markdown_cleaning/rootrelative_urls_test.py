cbd62f8a11e0d01befaa8a1991dc2dd77616ffd8
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-03-04 08:28:35 -0500

    format

--- websites/management/commands/markdown_cleaning/rootrelative_urls_test.py
+++ websites/management/commands/markdown_cleaning/rootrelative_urls_test.py
@@ -148,7 +148,7 @@ def test_rootrel_rule_handles_site_homeages_correctly(
             R"cool image ![alt text here](/courses/dep/site_one/blah/old_image_filename123.jpg) cool ",
             R'cool image {{< resource uuid-1 "alt text here" >}} cool ',
         ),
-        (   # Do not change cross-site images. They would need the AWS file...
+        (  # Do not change cross-site images. They would need the AWS file...
             "site_two",
             R"cool image ![alt text here](/courses/dep/site_one/blah/old_image_filename123.jpg) cool ",
             R"cool image ![alt text here](/courses/dep/site_one/blah/old_image_filename123.jpg) cool ",
