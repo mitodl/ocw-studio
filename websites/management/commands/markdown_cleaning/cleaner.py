0d79377c8ef4ef26bb0e68545f466e0d37fc1a45
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-03-08 10:26:48 -0500

    decouple cleaner from regex implementation

--- websites/management/commands/markdown_cleaning/cleaner.py
+++ websites/management/commands/markdown_cleaning/cleaner.py
@@ -14,28 +14,21 @@ from websites.models import WebsiteContent
 
 
 class WebsiteContentMarkdownCleaner:
-    """Facilitates regex-based replacements on WebsiteContent markdown.
+    """Facilitates replacements on WebsiteContent markdown.
 
     Args:
         rule: A MarkdownCleanupRule instance.
-
-    The given rule specifies a regex and is callable as a replacer function. The
-    rule will be called for every non-overlapping match of the regex and should
-    return the replacement string. This is similar to the `repl` argument of
-    `re.sub`, but is invoked with two arguments `(match, website_content)`,
-    instead of just `match`. Here `website_content` is a partial website_content
-    object.
-
-    If the pattern uses named capturing groups, these groups will be included as
-    csv columns by `write_to_csv()` method.
-
-    Note: Internally records all matches and replacement results for subsequent
-    writing to csv.
+    
+    The cleaner instance will make replacements using the given rule and record
+    information about each replacement. These records may be recovered via the
+    `write_matches_to_csv` method.
     """
 
     @dataclass
     class ReplacementMatch:
-        match: re.Match
+        """Stores data about each replacement made."""
+
+        original_text: str
         replacement: str
         content: WebsiteContent
         notes: Any  # should be a dataclass
@@ -51,36 +44,30 @@ class WebsiteContentMarkdownCleaner:
     ]
 
     def __init__(self, rule: MarkdownCleanupRule):
-        self.regex = self.compile_regex(rule.regex)
         self.rule = rule
-
-        self.text_changes: "list[WebsiteContentMarkdownCleaner.ReplacementMatch]" = []
+        self.replacement_matches: "list[WebsiteContentMarkdownCleaner.ReplacementMatch]" = (
+            []
+        )
         self.updated_website_contents: "list[WebsiteContent]" = []
         self.updated_sync_states: "list[ContentSyncState]" = []
 
-        def _replacer(match: re.Match, website_content: WebsiteContent):
-            result = rule(match, website_content)
-            if isinstance(result, str):
-                replacement = result
-                notes = self.rule.ReplacementNotes()
-            elif isinstance(result, tuple):
-                replacement, notes = result
-            else:
-                raise ValueError(
-                    "MarkdownCleanupRule instances should return strings or tuples when called"
-                )
-
-            self.text_changes.append(
-                self.ReplacementMatch(
-                    match=match,
-                    replacement=replacement,
-                    content=website_content,
-                    notes=notes,
-                )
+    def store_match_data(
+        self,
+        original_text: str,
+        replacement: str,
+        website_content: WebsiteContent,
+        notes,
+    ):
+        """Store match data for subsequent csv-generation."""
+        self.replacement_matches.append(
+            self.ReplacementMatch(
+                original_text,
+                replacement=replacement,
+                content=website_content,
+                notes=notes,
             )
-            return replacement
-
-        self.replacer = _replacer
+        )
+        return replacement
 
     def update_website_content_markdown(self, website_content: WebsiteContent):
         """
@@ -90,9 +77,8 @@ class WebsiteContentMarkdownCleaner:
         if not website_content.markdown:
             return
 
-        new_markdown = self.regex.sub(
-            lambda match: self.replacer(match, website_content),
-            website_content.markdown,
+        new_markdown = self.rule.transform_markdown(
+            website_content, self.store_match_data
         )
         if new_markdown != website_content.markdown:
             website_content.markdown = new_markdown
@@ -107,41 +93,27 @@ class WebsiteContentMarkdownCleaner:
                 sync_state.current_checksum = new_checksum
                 self.updated_sync_states.append(sync_state)
 
-    @classmethod
-    def compile_regex(cls, pattern):
-        """Compile `pattern` and validate that it has no named capturing groups
-        whose name would conflict with csv metadata fieldnames."""
-        compiled = re.compile(pattern)
-        for groupname in cls.csv_metadata_fieldnames:
-            if groupname in compiled.groupindex:
-                raise ValueError(
-                    f"Regex group name {groupname} is reserved for use by {cls.__name__}"
-                )
-        return compiled
-
     def write_matches_to_csv(self, path: str):
         """Write matches and replacements to csv."""
 
         with open(path, "w", newline="") as csvfile:
             fieldnames = [
                 *self.csv_metadata_fieldnames,
-                *self.regex.groupindex,
                 *(f.name for f in fields(self.rule.ReplacementNotes)),
             ]
             writer = csv.DictWriter(csvfile, fieldnames, quoting=csv.QUOTE_ALL)
             writer.writeheader()
             for change in self.text_changes:
                 row = {
-                    "original_text": change.match[0],
+                    "original_text": change.original_text,
                     "replacement": change.replacement,
-                    "has_changed": change.match[0] != change.replacement,
+                    "has_changed": change.original_text != change.replacement,
                     "replaced_on_site_name": change.content.website.name,
                     "replaced_on_site_short_id": change.content.website.short_id,
                     "replaced_on_page_uuid": change.content.text_id,
                     "replaced_on_page_url": get_rootrelative_url_from_content(
                         change.content
                     ),
-                    **change.match.groupdict(),
                     **asdict(change.notes),
                 }
                 writer.writerow(row)
--- websites/management/commands/markdown_cleaning/cleanup_rule.py
+++ websites/management/commands/markdown_cleaning/cleanup_rule.py
@@ -1,6 +1,7 @@
 import abc
+import re
 from dataclasses import dataclass
-from re import Match
+from typing import Any, Union
 
 from websites.models import WebsiteContent
 
@@ -16,6 +17,27 @@ class MarkdownCleanupRule(abc.ABC):
     def alias(cls) -> str:
         """Alias of the rule to be used in CLI"""
 
+    @abc.abstractmethod
+    def transform_markdown(self, website_content: WebsiteContent, on_match) -> str:
+        """Transform markdown associated with a website_content object.
+
+        Calls on_match(original_text, replacement, website_content, replacement_notes) for
+        each match.
+        """
+
+    @dataclass
+    class ReplacementNotes:
+        """Used to store notes about this replacement, e.g., the values of named
+        regex capturing groups.
+        """
+        pass
+
+
+class RegexpCleanupRule(MarkdownCleanupRule):
+    """
+    Regex-based replacements on markdown.
+    """
+
     @property
     @abc.abstractclassmethod
     def regex(cls) -> str:
@@ -23,14 +45,34 @@ class MarkdownCleanupRule(abc.ABC):
         The pattern to match for when making replacements.
         """
 
+    def __init__(self) -> None:
+        self.compiled = re.compile(self.regex)
+
     @abc.abstractmethod
-    def __call__(self, match: Match, website_cosntent: WebsiteContent):
+    def replace_match(self, match: re.Match, website_content: WebsiteContent):
         """
         Invoked for each match to the rule's regex and returns the replacement
         string. Similar to re.sub, but invoked with website_content argument
         also.
         """
 
-    @dataclass
-    class ReplacementNotes:
-        pass
+    def transform_markdown(self, website_content: WebsiteContent, on_match) -> str:
+        def _replacer(match: re.Match):
+            result = self.replace_match(match, website_content)
+            if isinstance(result, str):
+                replacement = result
+                notes = self.ReplacementNotes()
+            elif isinstance(result, tuple):
+                replacement, notes = result
+            else:
+                raise ValueError(
+                    "replace_match must return strings or tuples when called"
+                )
+
+            original_text = match[0]
+            on_match(original_text, replacement, website_content, notes)
+
+            return replacement
+
+        new_markdown = self.compiled.sub(_replacer, website_content.markdown)
+        return new_markdown
