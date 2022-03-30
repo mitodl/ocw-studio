b70f46cdbad0e98959dbedc0bc51d1ee7a414ccb
Matt Bertrand <mrbertrand@gmail.com>
2022-03-29 21:45:28 -0400

    fix tests

--- content_sync/decorators_test.py
+++ content_sync/decorators_test.py
@@ -9,8 +9,8 @@ from content_sync.decorators import single_task
 @pytest.mark.parametrize("input_arg", [None, "foo"])
 def test_single_task(mocker, has_lock, raise_block, input_arg):
     """single_task should only allow 1 instance of inner task to run at same time"""
-    mock_app = mocker.patch("content_sync.decorators.app")
-    mock_app.backend.client.lock.return_value.acquire.side_effect = [True, has_lock]
+    mock_app = mocker.patch("content_sync.decorators.get_redis_connection")
+    mock_app.return_value.lock.return_value.acquire.side_effect = [True, has_lock]
 
     func = mocker.Mock(__name__="testfunc")
     decorated_func = single_task(timeout=2, raise_block=raise_block)
@@ -21,7 +21,7 @@ def test_single_task(mocker, has_lock, raise_block, input_arg):
             decorated_func(func)(*args)
     else:
         decorated_func(func)(*args)
-    mock_app.backend.client.lock.assert_any_call(
+    mock_app.return_value.lock.assert_any_call(
         f"testfunc-id-{input_arg or 'single'}", timeout=2
     )
     assert func.call_count == (2 if has_lock else 1)
