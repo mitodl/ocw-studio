c5b971266d5110d1f8f62854800f9fd531253883
Wassaf-Shahzad <wassaf.shahzad@arbisoft.com>
2022-04-15 01:48:04 +0500

    added command to migrate testimonials to stories

---
+++ websites/management/commands/rename_testimonials.py
@@ -0,0 +1,62 @@
+"""Updates multiple fields of content based on starter"""
+from django.db import transaction
+from django.core.management import BaseCommand, CommandError
+from django.core.exceptions import FieldDoesNotExist
+from websites.models import WebsiteContent
+
+
+class Command(BaseCommand):
+
+    help = __doc__
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "starter",
+            help="The WebsiteStarter slug to process",
+        )
+
+        parser.add_argument(
+            "-t",
+            "--type",
+            dest="type",
+            default="testimonials",
+            help="The type of content to process",
+        )
+
+        parser.add_argument(
+            "-k",
+            "--keys",
+            dest="keys",
+            nargs="+",
+            default=["type", "dirpath"],
+        )
+
+        parser.add_argument(
+            "-d",
+            "--data",
+            dest="data",
+            nargs="+",
+            default=["stories", "content/stories"],
+        )
+
+    def is_valid(self, fields, values):
+        return len(fields) == len(values)
+
+    def handle(self, *args, **options):
+        
+        fields = options.get("keys")
+        values = options.get("data")
+        starter = options.get("starter")
+        page_type = options.get("type")
+
+        if not self.is_valid(fields, values):
+            raise CommandError("The number of arguments for keys should match data")
+
+        updated_data = {x: y for x, y in zip(fields, values)}
+        with transaction.atomic():
+            try:
+                WebsiteContent.objects.filter(
+                    website__starter__slug=starter, type=page_type
+                ).update(**updated_data)
+            except FieldDoesNotExist as e:
+                raise CommandError(e)
