47615b017e9b3c84a30722b2efa9e0298f6f298d
Wassaf-Shahzad <wassaf.shahzad@arbisoft.com>
2022-04-20 02:07:19 +0500

    added commad to update departments

---
+++ websites/management/commands/update_departments.py
@@ -0,0 +1,28 @@
+from functools import reduce
+
+from django.core.management import BaseCommand
+from django.db import transaction
+from django.db.models import Q
+
+from websites.models import Website
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        filter_set = ["21W", "CMS"]
+        filter_query_set = [
+            "name__istartswith",
+            "short_id__istartswith",
+        ]
+
+        query_set = Website.objects.filter(
+            reduce(
+                lambda x, y: x | y,
+                [Q(**{key: value}) for key, value in zip(filter_query_set, filter_set)],
+            )
+        )
+
+        with transaction.atomic():
+            for website in query_set.iterator():
+                website.metadata = {**website.metadata, **{"departments": ["CMS-W"]}}
+                website.save()
