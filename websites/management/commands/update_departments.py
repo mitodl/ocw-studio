8638745bd3230543539af3b4d5b2a20e9f1225ee
Wassaf-Shahzad <wassaf.shahzad@arbisoft.com>
2022-04-26 13:11:35 +0500

    updated to use department number

--- websites/management/commands/update_departments.py
+++ websites/management/commands/update_departments.py
@@ -4,29 +4,30 @@ from django.core.management import BaseCommand
 from django.db import transaction
 from django.db.models import Q
 
-from websites.models import Website
+from websites.models import WebsiteContent
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         filter_set = ["21W", "CMS"]
         filter_query_set = [
-            "name__istartswith",
-            "short_id__istartswith",
+            "website__name__istartswith",
+            "website__short_id__istartswith",
         ]
 
-        query_set = Website.objects.filter(
+        query_set = WebsiteContent.objects.filter(
             reduce(
                 lambda x, y: x | y,
                 [Q(**{key: value}) for key, value in zip(filter_query_set, filter_set)],
-            )
+            ),
         )
 
+        query_set = query_set.filter(type="sitemetadata")
         with transaction.atomic():
-            for website in query_set.iterator():
-                if not website.metadata["departments"]:
-                    website.metadata["departments"] = [
-                        *website.metadata["departments"],
+            for sitemetadata in query_set.iterator():
+                if not sitemetadata.metadata["department_numbers"]:
+                    sitemetadata.metadata["department_numbers"] = [
+                        *sitemetadata.metadata["department_numbers"],
                         "CMS-W",
                     ]
-                    website.save()
+                    sitemetadata.save()
