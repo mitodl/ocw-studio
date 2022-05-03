960cdc792ba743a84409c381daef72dcdc9a8280
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-05-03 10:40:12 -0400

    install @reduxjs/toolkit

--- static/js/reducers/index.ts
+++ static/js/reducers/index.ts
@@ -1,4 +1,4 @@
-import { combineReducers } from "redux"
+import { combineReducers } from "@reduxjs/toolkit"
 import { entitiesReducer, queriesReducer } from "redux-query"
 
 const rootReducer = combineReducers({
