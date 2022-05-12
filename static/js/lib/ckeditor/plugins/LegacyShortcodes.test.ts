fc199e6a667c82f090d30e7714f22e4a76438f43
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-05-12 13:52:31 -0400

    improve legacy shortcode preservation
    In particular:
    - flexibility in closing slash position
    - allow shortcodes in argument values. They won't display right in Hugo, but at least now they get preserved.

--- static/js/lib/ckeditor/plugins/LegacyShortcodes.test.ts
+++ static/js/lib/ckeditor/plugins/LegacyShortcodes.test.ts
@@ -5,7 +5,7 @@ import LegacyShortcodes from "./LegacyShortcodes"
 import { LEGACY_SHORTCODES } from "./constants"
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph"
 
-const quizTestMD = `{{< quiz_multiple_choice questionId="Q1_div" >}}{{< quiz_choices >}}{{< quiz_choice isCorrect="false" >}}sound, satisfiable{{< /quiz_choice >}}{{< quiz_choice isCorrect="false" >}}valid, satisfiable{{< /quiz_choice >}}{{< quiz_choice isCorrect="true" >}}sound, valid{{< /quiz_choice >}}{{< quiz_choice isCorrect="false" >}}valid, true{{< /quiz_choice >}}{{< /quiz_choices >}}{{< quiz_solution / >}}{{< /quiz_multiple_choice >}}`
+const quizTestMD = `{{< quiz_multiple_choice questionId="Q1_div" >}}{{< quiz_choices >}}{{< quiz_choice isCorrect="false" >}}sound, satisfiable{{< /quiz_choice >}}{{< quiz_choice isCorrect="false" >}}valid, satisfiable{{< /quiz_choice >}}{{< quiz_choice isCorrect="true" >}}sound, valid{{< /quiz_choice >}}{{< quiz_choice isCorrect="false" >}}valid, true{{< /quiz_choice >}}{{< /quiz_choices >}}{{< quiz_solution >}}{{< /quiz_multiple_choice >}}`
 
 const getEditor = createTestEditor([Paragraph, LegacyShortcodes, Markdown])
 
@@ -31,20 +31,42 @@ describe("ResourceEmbed plugin", () => {
   test.each(LEGACY_SHORTCODES)(
     "should take in and return %p closing shortcode",
     async shortcode => {
-      const md = `{{< /${shortcode} >}}`
-      const editor = await getEditor(md)
+      const md1 = `{{< /${shortcode} >}}`
+      const editor1 = await getEditor(md1)
       // @ts-ignore
-      expect(editor.getData()).toBe(md)
+      expect(editor1.getData()).toBe(md1)
+
+      /**
+       * Hugo documentation uses the first version so it should be preferred.
+       * But Hugo accepts this version and image-gallery uses it.
+       * The editor will normalize it to the first version.
+       */
+      const md2 = `{{</ ${shortcode} >}}`
+      const editor2 = await getEditor(md2)
+      // @ts-ignore
+      expect(editor2.getData()).toBe(md1)
     }
   )
 
   test.each(LEGACY_SHORTCODES)(
-    "should take in and return %p shortcode with arguments",
+    "should take in and return %p shortcode with positional parameters",
     async shortcode => {
-      const md = `{{< ${shortcode} arguments foo=123 html=<for some reason/> >}}`
+      const md = `{{< ${shortcode} arguments "and chemistry H{{< sub 2 >}}0" >}}`
+      const expected = `{{< ${shortcode} "arguments" "and chemistry H{{< sub 2 >}}0" >}}`
       const editor = await getEditor(md)
       // @ts-ignore
-      expect(editor.getData()).toBe(md)
+      expect(editor.getData()).toBe(expected)
+    }
+  )
+
+  test.each(LEGACY_SHORTCODES)(
+    "should take in and return %p shortcode with named parameters",
+    async shortcode => {
+      const md = `{{< ${shortcode} foo=123 html="<for some reason/>" >}}`
+      const expected = `{{< ${shortcode} foo="123" html="<for some reason/>" >}}`
+      const editor = await getEditor(md)
+      // @ts-ignore
+      expect(editor.getData()).toBe(expected)
     }
   )
 
