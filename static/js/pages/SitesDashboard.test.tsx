8df51e5418dc4b80b19946f95ad5603abd9cb1d8
Muhammad Abdurrehman <muhammad.abdurrehman@arbisoft.com>
2022-05-17 20:13:20 +0500

    unused function removed

--- static/js/pages/SitesDashboard.test.tsx
+++ static/js/pages/SitesDashboard.test.tsx
@@ -3,7 +3,7 @@ import SitesDashboard from "./SitesDashboard"
 import { newSiteUrl, siteApiListingUrl, siteDetailUrl } from "../lib/urls"
 import { WebsiteListingResponse } from "../query-configs/websites"
 import { wait } from "../test_util"
-import { makeWebsites, makeWebsiteDetail } from "../util/factories/websites"
+import { makeWebsites } from "../util/factories/websites"
 import IntegrationTestHelper, {
   TestRenderer
 } from "../util/integration_test_helper_old"
