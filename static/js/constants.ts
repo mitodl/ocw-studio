0ad89df9f7eb98cf27485810a5442ef123262e05
Wassaf-Shahzad <wassaf.shahzad@arbisoft.com>
2022-06-13 17:57:20 +0500

    formatted code

--- static/js/constants.ts
+++ static/js/constants.ts
@@ -15,9 +15,9 @@ export const ROLE_OWNER = "owner"
 
 export const ROLE_LABELS = {
   [ROLE_GLOBAL]: "Administrator",
-  [ROLE_ADMIN]: "Administrator",
+  [ROLE_ADMIN]:  "Administrator",
   [ROLE_EDITOR]: "Editor",
-  [ROLE_OWNER]: "Owner"
+  [ROLE_OWNER]:  "Owner"
 }
 
 export const EDITABLE_ROLES = [ROLE_ADMIN, ROLE_EDITOR]
