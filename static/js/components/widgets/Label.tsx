822b0dfb191d22d40510737bda66889d19c1be55
Paul Tylkin <ptylkin@mit.edu>
2022-06-14 15:24:25 -0400

    Fixing linting errors

--- static/js/components/widgets/Label.tsx
+++ static/js/components/widgets/Label.tsx
@@ -9,17 +9,17 @@ export interface Props {
 /**
  * A component for showing a read-only label
  */
-const Label: React.FC<Props> = (props) => {
+const Label: React.FC<Props> = props => {
   const { name, value } = props
-  const fileInputName = name || "file"
   return (
     <div className="py-2">
-    {value && !(value instanceof File) ? (
-      <div className="current-file">
+      {value && !(value instanceof File) ? (
+        <div className="current-file">
         Current file: {filenameFromPath(value)}
-      </div>
-    ) : null}
+        </div>
+      ) : null}
     </div>
-    )}
+  )
+}
 
 export default Label
