3da9f1fd6f29a4fd184e101c63a392efe6269faf
Carey Gumaer <gumaerc@mit.edu>
2022-09-02 19:17:01 -0400

    linting

--- content_sync/pipelines/concourse.py
+++ content_sync/pipelines/concourse.py
@@ -521,7 +521,7 @@ class ThemeAssetsPipeline(GeneralPipeline, BaseThemeAssetsPipeline):
         self.upsert_config(config_str, self.PIPELINE_NAME)
 
 
-class MassBuildSitesPipeline(BaseMassBuildSitesPipeline, GeneralPipeline):
+class MassBuildSitesPipeline(BaseMassBuildSitesPipeline, GeneralPipeline):  # pylint: disable=too-many-instance-attributes
     """Specialized concourse pipeline for mass building multiple sites"""
 
     PIPELINE_NAME = BaseMassBuildSitesPipeline.PIPELINE_NAME
