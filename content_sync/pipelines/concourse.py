42edad50e7f1413a8128c8b15a284bbee56bdbf5
Muhammad Abdurrehman <muhammad.abdurrehman@arbisoft.com>
2022-10-26 16:08:21 +0500

    added ocw hugo themes sentry dsn

--- content_sync/pipelines/concourse.py
+++ content_sync/pipelines/concourse.py
@@ -467,6 +467,7 @@ class SitePipeline(BaseSitePipeline, GeneralPipeline):
                     f" --endpoint-url {DEV_ENDPOINT_URL}" if is_dev() else "",
                 )
                 .replace("((resource-base-url))", resource_base_url or "")
+                .replace("((ocw-hugo-themes-sentry-dsn))", settings.OCW_HUGO_THEMES_SENTRY_DSN)
             )
             self.upsert_config(config_str, pipeline_name)
 
@@ -669,6 +670,7 @@ class MassBuildSitesPipeline(
                     and self.PROJECTS_BRANCH == settings.GITHUB_WEBHOOK_BRANCH
                 ),
             )
+            .replace("((ocw-hugo-themes-sentry-dsn))", settings.OCW_HUGO_THEMES_SENTRY_DSN)
         )
         self.upsert_config(config_str, self.PIPELINE_NAME)
 
