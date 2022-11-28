65a39da221e480b4466040081c7f44d9c6e1fe6e
Carey P Gumaer <gumaerc@mit.edu>
2022-11-28 02:05:01 -0500

    filter out mp4 files in the offline mass-build-sites (#1579)
    * filter out mp4 files in the offline build
    
    * fix test
    
    * remove unnecessary quotes

--- content_sync/pipelines/concourse_test.py
+++ content_sync/pipelines/concourse_test.py
@@ -656,7 +656,7 @@ def test_upsert_mass_build_pipeline(
         assert f"HUGO_RESULT=$(hugo --config ../ocw-hugo-projects/$STARTER_SLUG/config.yaml --themesDir ../ocw-hugo-themes/ {build_drafts} --quiet) || HUGO_RESULT=1"
         if settings.ENVIRONMENT == "dev":
             assert (
-                f"STUDIO_S3_RESULT=$(aws s3{endpoint_url} sync s3://{settings.AWS_STORAGE_BUCKET_NAME}/$S3_PATH ./content/static_resources --only-show-errors) || STUDIO_S3_RESULT=1"
+                f"STUDIO_S3_RESULT=$(aws s3{endpoint_url} sync s3://{settings.AWS_STORAGE_BUCKET_NAME}/$S3_PATH ./content/static_resources --exclude *.mp4 --only-show-errors) || STUDIO_S3_RESULT=1"
                 in config_str
             )
     else:
