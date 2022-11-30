67c56ad548e9a97c112ffec8304eb9366adff581
pt2302 <1553279+pt2302@users.noreply.github.com>
2022-11-30 16:26:41 -0500

    Adding management command to delete objects with missing type (#1587)
    * Adding management command to delete objects with missing type
    
    * Adding message about number of objects deleted

---
+++ websites/management/commands/delete_missing_type_objects.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+from safedelete.models import HARD_DELETE
+
+from websites.models import WebsiteContent
+
+
+class Command(BaseCommand):
+    """Delete objects in the Django database that are missing a type"""
+
+    help = __doc__
+
+    def handle(self, *args, **options):
+        missing_type_content = WebsiteContent.objects.all(force_visibility=True).filter(
+            type=""
+        )
+        for content in missing_type_content:
+            content.delete(force_policy=HARD_DELETE)
+        self.stdout.write(
+            str(len(missing_type_content)) + " objects with missing type deleted.\n"
+        )
