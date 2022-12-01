acbffe943b707df0aa672eb4dcecc5620c8be2d4
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-12-01 17:29:29 -0500

    Subscripts and superscripts (#1588)
    * enable subsup-scripts
    
    * move resetTurndown
    
    * fix typeerrors, add tests, better html inclusion
    
    * clarify comments around _keep
    
    * Add a comment
    
    * rename placeholder
    
    * add another test
    
    * fix issue with zwsp replacement

--- static/js/lib/ckeditor/plugins/ResourceLinkMarkdownSyntax.test.ts
+++ static/js/lib/ckeditor/plugins/ResourceLinkMarkdownSyntax.test.ts
@@ -110,4 +110,66 @@ describe("ResourceLink plugin", () => {
     )}">{{&lt; sup 2 &gt;}}</a> Woof</p>`
     markdownTest(editor, md, html)
   })
+
+  it.each([
+    {
+      allowedHtml: ["sup"],
+      sup:         (x: string) => `<sup>${x}</sup>`
+    },
+    {
+      allowedHtml: [""],
+      sup:         (x: string) => x
+    }
+  ])(
+    "Preserves superscripts in the link title iff sub tag is permitted",
+    async ({ allowedHtml, sup }) => {
+      const editor = await getEditor("", {
+        "markdown-config": { allowedHtml }
+      })
+      const { md2html, html2md } = getConverters(editor)
+
+      const html = `<p>Dogs <sup>x</sup> <a class="resource-link" data-uuid="${encode(
+        "uuid123"
+      )}">Einstein says E=mc<sup>2</sup></a> Woof</p>`
+
+      const md =
+        'Dogs <sup>x</sup> {{% resource_link "uuid123" "Einstein says E=mc<sup>2</sup>" %}} Woof'
+      /**
+       * md -> html always keeps extra tags
+       */
+      expect(md2html(md)).toBe(html)
+
+      /**
+       * html -> md only keeps extra tags if specified by allowedHtml
+       */
+      expect(html2md(html)).toBe(
+        `Dogs ${sup("x")} {{% resource_link "uuid123" "Einstein says E=mc${sup(
+          "2"
+        )}" %}} Woof`
+      )
+    }
+  )
+
+  /**
+   * It seems that this scenario cannot actually be constructed in CKEditor.
+   * CKEditor treats subscripts, subscripts, links, and resource links as
+   * attributes on a text node, not as some sort of tree structure (like html)
+   * with nesting. It's unclear how the conversion order from "attributes on
+   * text node" to an HTML tree is performed, but it seems that resource_link
+   * always ends up on the outside.
+   *
+   * Still, nice that this works.
+   */
+  it("Preserves resource links inside superscripts if sup enabled", async () => {
+    const editor = await getEditor("", {
+      "markdown-config": { allowedHtml: ["sup"] }
+    })
+    const md =
+      'Cool reference<sup>{{% resource_link "uuid123" "\\[1\\]" %}}</sup>'
+    const html = `<p>Cool reference<sup><a class="resource-link" data-uuid="${encode(
+      "uuid123"
+    )}">[1]</a></sup></p>`
+
+    markdownTest(editor, md, html)
+  })
 })
--- static/js/lib/ckeditor/turndown.ts
+++ static/js/lib/ckeditor/turndown.ts
@@ -111,3 +111,121 @@ itemRule.replacement = (
     prefix + content + (node.nextSibling && !/\n$/.test(content) ? "\n" : "")
   )
 }
+
+const BASE_TURNDOWN_RULES = [...turndownService.rules.array]
+const BASE_TURNDOWN_KEEP = [
+  // @ts-expect-error `_keep` is not part of Turndown's public API, see `resetTurndownService` for more.
+  ...turndownService.rules._keep
+]
+const BASE_TURNDOWN_KEEP_REPLACEMENT = turndownService.rules.keepReplacement
+
+/**
+ * CKEditor's markdown plugin uses a single Turndown instance. We occasionally
+ * want to reset it to its initial state (e.g., for different editor instances).
+ */
+export const resetTurndownService = () => {
+  turndownService.rules.array = [...BASE_TURNDOWN_RULES]
+
+  /**
+   * We need to access `_keep` in order to "reset" what HTML tags Turndown will
+   * keep when converting HTML to Markdown. Turndown has a public API for
+   * adding tags to its "keep" list, but not for removing tags or resetting its
+   * list.
+   */
+  // @ts-expect-error `_keep` is not part of Turndown's public API, see above
+  turndownService.rules._keep = [...BASE_TURNDOWN_KEEP]
+  turndownService.rules.keepReplacement = BASE_TURNDOWN_KEEP_REPLACEMENT
+}
+
+/**
+ * This class contains a few helpers that improve upon the default way in which
+ * Turndown includes raw HTML tags in the markdown output.
+ *
+ * When converting HTML -> MD, Turndown generally ignores HTML tags that have no
+ * markdown equivalent. (E.g., "span", "div", "sup", "sub", ...). This behavior
+ * can be customized in two ways:
+ *  1. turndown's `keep` method tells Turndown which rules to keep
+ *  2. turndown's `keepReplacement` option tells Turndown HOW to replace those tags
+ *
+ * The default behavior of `keepReplacement` has two drawbacks:
+ *
+ * Issue #1
+ * -----------
+ * The default behavior can result in HTML tags beginning a markdown line that
+ * would otherwise be a Markdown paragraph. For example:
+ * ```js
+ * const tds = new TurndownService()
+ * tds.keep(["sup"])
+ * tds.turndown("<p><sup>2</sup> Hello <strong>world</strong>!</p>")
+ * // => "<sup>2</sup> Hello **world**!"
+ * ```
+ * The output above is bad because when an HTML tag begins a markdown line, it
+ * begins a *block* of markdown, and subsequent characters on that line are
+ * NOT treated as Markdown. Hence the `**` above would be rendered as literal
+ * asterisks, not as bold.
+ *
+ * Issue #2
+ * ------------
+ * The default behavior is that the *content* of an HTML tag is not converted
+ * to Markdown. This is problematic for tags that contain shortcode HTML. For
+ * example, `<sup><a class="resource-link" data-uuid ... >2</a></sup>` should
+ * really needs its content to be converted to markdown in order for Hugo to
+ * recognize it as a shortcode.
+ */
+class TurndownHtmlHelpers {
+  private turndownInstance: Turndown
+
+  constructor(turndown: Turndown) {
+    this.turndownInstance = turndown
+  }
+
+  /**
+   * This function improves upon Turndown's default rule for including raw HTML
+   * tags in Markdown. In particular, it:
+   *  - ensures that the child content of an inline HTML node is also converted, which
+   *    we need in case the child content contains shortcode.
+   *  - ensures that an inline HTML tag never begins a line of Markdown.
+   *
+   * Should be used alongside {@link TurndownHtmlHelpers.turndown}
+   */
+  keepReplacer: Turndown.ReplacementFunction = (content, node) => {
+    /**
+     * "isBlock" is a Turndown-specific addition to the DOM node interface.
+     * It appears to be part of their public API in that the author recommends
+     * using it in several issues.
+     */
+    const el = node as HTMLElement & { isBlock: boolean }
+
+    if (el.isBlock) {
+      throw new Error("Inclusion of block content not yet supported.")
+    }
+
+    /**
+     * Now convert the node's child content to Markdown.
+     * This addresses Issue #2 above.
+     */
+    const clone = el.cloneNode() as HTMLElement
+    clone.innerHTML = this.turndownInstance.turndown(el.innerHTML)
+
+    /**
+     * To address Issue #1, we'll mark places where HTML is inserted. Later, we
+     * can remove all the markings except the ones that begin a new line.
+     */
+    return `<raw_inline></raw_inline>${clone.outerHTML}`
+  }
+
+  /**
+   * Convert HTML to markdown and insert a zero-width space before any HTML
+   * tag that begins what would otherwise be a Markdown paragraph. Should
+   * be used alongside {@link TurndownHtmlHelpers.keepReplacer}.
+   *
+   * See {@link TurndownHtmlHelpers} for more details.
+   */
+  turndown = (html: string) =>
+    this.turndownInstance
+      .turndown(html)
+      .replace(/(^|(?<=\n))[ ]*<raw_inline><\/raw_inline>/g, "\u200b")
+      .replace(/<raw_inline><\/raw_inline>/g, "")
+}
+
+export const turndownHtmlHelpers = new TurndownHtmlHelpers(turndownService)
