ff22cdcc48050e2bb4dde368c04f0ee534cad17b
pt2302 <1553279+pt2302@users.noreply.github.com>
2022-12-05 18:44:59 -0500

    Updating test with mock useLocation value

--- static/js/components/StudioList.test.tsx
+++ static/js/components/StudioList.test.tsx
@@ -2,6 +2,20 @@ import React from "react"
 import { shallow } from "enzyme"
 import { StudioListItem } from "./StudioList"
 
+const mockUseLocationValue = {
+  pathname: "/test/route",
+  search:   "",
+  hash:     "",
+  state:    null
+}
+
+jest.mock("react-router", () => ({
+  ...(jest.requireActual("react-router") as any),
+  useLocation: jest.fn().mockImplementation(() => {
+    return mockUseLocationValue
+  })
+}))
+
 describe("StudioListItem", () => {
   it("does not have hover-pointer class if onClick not provided", () => {
     const wrapper = shallow(
