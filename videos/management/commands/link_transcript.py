59465f79a293fab0e66eb6654ad64235d7a14b16
pt2302 <1553279+pt2302@users.noreply.github.com>
2022-12-12 15:29:03 -0500

    Adding management command for linking transcript to video

---
+++ videos/management/commands/link_transcript.py
@@ -0,0 +1,33 @@
+from django.core.management import BaseCommand
+from websites.models import WebsiteContent
+
+
+class Command(BaseCommand):
+    """Link the video resource with the UUID video_uuid to the transcript resource with transcript_uuid"""
+
+    help = __doc__
+
+    def add_arguments(self, parser):
+
+        parser.add_argument(
+            "--video_uuid",
+            dest="video_uuid",
+            help="text_id of video resource object to be updated",
+            required=True,
+        )
+
+        parser.add_argument(
+            "--transcript_uuid",
+            dest="transcript_uuid",
+            help="text_id of transcript resource object to be linked",
+            required=True,
+        )
+
+    def handle(self, *args, **options):
+        video = WebsiteContent.objects.get(text_id=options["video_uuid"])
+        transcript = WebsiteContent.objects.get(text_id=options["transcript_uuid"])
+        video.metadata["video_files"][
+            "video_captions_file"
+        ] = transcript.drivefile_set.first().s3_key
+        video.save()
+        self.stdout.write("Transcript successfully linked to video.\n")
