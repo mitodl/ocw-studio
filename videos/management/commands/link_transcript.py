38d2123840a1f8c00968920d1c576c41d536e921
pt2302 <1553279+pt2302@users.noreply.github.com>
2022-12-12 19:04:38 -0500

    Linking both captions and transcript to video resource

--- videos/management/commands/link_transcript.py
+++ videos/management/commands/link_transcript.py
@@ -3,7 +3,7 @@ from websites.models import WebsiteContent
 
 
 class Command(BaseCommand):
-    """Link the video resource with the UUID video_uuid to the transcript resource with transcript_uuid"""
+    """Link the video resource with the UUID video_uuid to the captions resource with captions_uuid and downloadable transcript with transcript_uuid"""
 
     help = __doc__
 
@@ -17,17 +17,28 @@ class Command(BaseCommand):
         )
 
         parser.add_argument(
+            "--captions_uuid",
+            dest="captions_uuid",
+            help="text_id of captions resource object to be linked",
+            required=True,
+        )
+
+        parser.add_argument(
             "--transcript_uuid",
             dest="transcript_uuid",
-            help="text_id of transcript resource object to be linked",
+            help="text_id of downloadable transcript PDF resource object to be linked",
             required=True,
         )
 
     def handle(self, *args, **options):
         video = WebsiteContent.objects.get(text_id=options["video_uuid"])
-        transcript = WebsiteContent.objects.get(text_id=options["transcript_uuid"])
+        captions = WebsiteContent.objects.get(text_id=options["captions_uuid"])
         video.metadata["video_files"][
             "video_captions_file"
+        ] = captions.drivefile_set.first().s3_key
+        transcript = WebsiteContent.objects.get(text_id=options["transcript_uuid"])
+        video.metadata["video_files"][
+            "video_transcript_file"
         ] = transcript.drivefile_set.first().s3_key
         video.save()
-        self.stdout.write("Transcript successfully linked to video.\n")
+        self.stdout.write("Captions and transcript successfully linked to video.\n")
