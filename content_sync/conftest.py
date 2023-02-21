1e8793696f7ec635a6249adf14a40e883b01dfb9
Carey Gumaer <gumaerc@mit.edu>
2023-02-21 13:02:28 -0500

    whitespace

--- content_sync/conftest.py
+++ content_sync/conftest.py
@@ -44,7 +44,7 @@ def required_concourse_settings(settings):
     settings.AWS_PREVIEW_BUCKET_NAME = "preview_bucket"
     settings.AWS_PUBLISH_BUCKET_NAME = "publish_bucket"
     settings.AWS_OFFLINE_PREVIEW_BUCKET_NAME = "offline_preview_bucket"
-    settings.AWS_OFFLINE_PUBLISH_BUCKET_NAME = "offline_publish_bucket"    
+    settings.AWS_OFFLINE_PUBLISH_BUCKET_NAME = "offline_publish_bucket"
     settings.AWS_STORAGE_BUCKET_NAME = "storage_bucket"
     settings.GIT_BRANCH_PREVIEW = "preview"
     settings.GIT_BRANCH_RELEASE = "release"
