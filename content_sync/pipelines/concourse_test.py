6b477ee1add2c35610c24169d2c67c68a4f4c8b5
Carey Gumaer <gumaerc@mit.edu>
2023-05-04 15:35:34 -0400

    add a test to make sure the mp4 rm command is inserted properly into pipeline definitions for all sites

--- content_sync/pipelines/concourse_test.py
+++ content_sync/pipelines/concourse_test.py
@@ -357,6 +357,9 @@ def test_upsert_website_pipelines(
             in config_str
         )
         assert (
+            f"rm -rf ./output-online/{website.name}/*.mp4" in config_str
+        )
+        assert (
             f"aws s3 {expected_endpoint_prefix}sync course-markdown/output-online s3://{bucket}/ --metadata site-id={website.name}"
             in config_str
         )
@@ -365,6 +368,9 @@ def test_upsert_website_pipelines(
         assert (
             "cp -r ../build-artifacts/static_shared/. ./static/static_shared/"
             in config_str
+        )
+        assert (
+            f"rm -rf ./output-online/*.mp4" in config_str
         )
         assert (
             f"aws s3 {expected_endpoint_prefix}sync course-markdown/output-online s3://{bucket}/{website.url_path} --metadata site-id={website.name} --delete"
