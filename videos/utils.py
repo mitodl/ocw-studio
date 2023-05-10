425fd6c2a803752f90bf7b35162b961984b5de58
Muhammad Ghulam Fakhar ud din <muhammad.fakhar@arbisoft.com>
2023-05-10 22:09:22 +0500

    Added os.sep

--- videos/utils.py
+++ videos/utils.py
@@ -23,7 +23,7 @@ def generate_s3_path(file_or_webcontent, website):
     elif new_filename_ext == "pdf":
         new_filename += "_transcript"
 
-    new_filename = f"{new_filename.strip('/')}.{new_filename_ext}"
+    new_filename = f"{new_filename.strip(os.sep)}.{new_filename_ext}"
 
     return os.path.join(website.s3_path.strip(os.sep), new_filename)
 
