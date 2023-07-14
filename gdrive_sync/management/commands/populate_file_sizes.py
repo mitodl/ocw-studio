708019232d4f5741b674bf8d9e255dab29c982d9
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2023-07-14 11:40:42 +0500

    chore: create populate_file_sizes command (#1861)
    * Create populate_file_sizes.py
    
    * Fix typo
    
    * refactor
    
    * add request timeout
    
    * refactor
    
    * mark mock_s3
    
    * remove chord

---
+++ gdrive_sync/management/commands/populate_file_sizes.py
@@ -0,0 +1,54 @@
+"""Populate file_size meta field for resources."""
+from django.db.models import Q
+
+from gdrive_sync.tasks import populate_file_sizes_bulk
+from main.management.commands.filter import WebsiteFilterCommand
+from websites.models import Website
+
+
+class Command(WebsiteFilterCommand):
+    """
+    Populate file_size metadata for file resources.
+
+    Usage Examples
+
+    ./manage.py populate_file_sizes
+    ./manage.py populate_file_sizes --filter course-id
+    ./manage.py populate_file_sizes --filter course-id --override-existing
+    """
+
+    help = __doc__
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            "--override-existing",
+            dest="override_existing",
+            action="store_true",
+            default=False,
+            help="Override existing file_size values",
+        )
+
+    def handle(self, *args, **options):
+        super().handle(*args, **options)
+
+        if self.filter_list:
+            website_queryset = Website.objects.filter(
+                Q(name__in=self.filter_list) | Q(short_id__in=self.filter_list)
+            )
+        else:
+            website_queryset = Website.objects.all()
+
+        website_names = list(website_queryset.values_list("name", flat=True))
+
+        self.stdout.write("Scheduling populate_file_sizes_bulk...")
+
+        task = populate_file_sizes_bulk.delay(
+            website_names, options["override_existing"]
+        )
+
+        self.stdout.write("Waiting on task...")
+
+        task.get()
+
+        self.stdout.write("Done")
