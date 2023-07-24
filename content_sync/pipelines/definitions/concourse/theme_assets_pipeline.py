1603484fb5c74915fe25791995cdad1225e9c5df
Carey Gumaer <gumaerc@mit.edu>
2023-07-24 18:12:52 -0400

    remove unused properties and prefix hidden ones with an underscore so they don't show up in the output

--- content_sync/pipelines/definitions/concourse/theme_assets_pipeline.py
+++ content_sync/pipelines/definitions/concourse/theme_assets_pipeline.py
@@ -20,11 +20,6 @@ from content_sync.pipelines.definitions.concourse.common.image_resources import 
     AWS_CLI_REGISTRY_IMAGE,
     OCW_COURSE_PUBLISHER_REGISTRY_IMAGE,
 )
-from content_sync.pipelines.definitions.concourse.common.resource_types import (
-    HttpResourceType,
-    KeyvalResourceType,
-    S3IamResourceType,
-)
 from content_sync.pipelines.definitions.concourse.common.resources import (
     GitResource,
     OpenDiscussionsResource,
@@ -42,18 +37,14 @@ CLI_ENDPOINT_URL = f" --endpoint-url {DEV_ENDPOINT_URL}" if is_dev() else ""
 
 
 class ThemeAssetsPipelineDefinition(Pipeline):
-    http_resource_type = HttpResourceType()
-    keyval_resource_type = KeyvalResourceType()
-    s3_iam_resource_type = S3IamResourceType()
+    _build_theme_assets_job_identifier = Identifier("build-theme-assets-job")
+    _build_ocw_hugo_themes_identifier = Identifier("build-ocw-hugo-themes-task")
+    _upload_theme_assets_task_identifier = Identifier("upload-theme-assets-task")
+    _clear_draft_cdn_cache_task_identifier = Identifier("clear-draft-cdn-cache-task")
+    _clear_live_cdn_cache_identifier = Identifier("clear-live-cdn-cache-task")
 
-    build_theme_assets_job_identifier = Identifier("build-theme-assets-job")
-    build_ocw_hugo_themes_identifier = Identifier("build-ocw-hugo-themes-task")
-    upload_theme_assets_task_identifier = Identifier("upload-theme-assets-task")
-    clear_draft_cdn_cache_task_identifier = Identifier("clear-draft-cdn-cache-task")
-    clear_live_cdn_cache_identifier = Identifier("clear-live-cdn-cache-task")
-
-    open_discussions_resource = OpenDiscussionsResource()
-    slack_resource = SlackAlertResource()
+    _open_discussions_resource = OpenDiscussionsResource()
+    _slack_resource = SlackAlertResource()
 
     class Config:
         arbitrary_types_allowed = True
@@ -81,7 +72,7 @@ class ThemeAssetsPipelineDefinition(Pipeline):
                 trigger=(not is_dev()),
             ),
             TaskStep(
-                task=self.build_ocw_hugo_themes_identifier,
+                task=self._build_ocw_hugo_themes_identifier,
                 config=TaskConfig(
                     platform="linux",
                     image_resource=OCW_COURSE_PUBLISHER_REGISTRY_IMAGE,
@@ -96,8 +87,8 @@ class ThemeAssetsPipelineDefinition(Pipeline):
                         path="sh",
                         args=[
                             "-exc",
-                            f"""
-                            cd {OCW_HUGO_THEMES_GIT_IDENTIFIER}
+                            """
+                            cd ocw-hugo-themes
                             yarn install --immutable
                             npm run build:webpack
                             npm run build:githash
@@ -107,7 +98,7 @@ class ThemeAssetsPipelineDefinition(Pipeline):
                 ),
             ),
             TaskStep(
-                task=self.upload_theme_assets_task_identifier,
+                task=self._upload_theme_assets_task_identifier,
                 timeout="20m",
                 attempts=3,
                 config=TaskConfig(
@@ -128,31 +119,31 @@ class ThemeAssetsPipelineDefinition(Pipeline):
                         args=[
                             "-exc",
                             f"""
-                            aws s3{CLI_ENDPOINT_URL} cp {OCW_HUGO_THEMES_GIT_IDENTIFIER}/base-theme/dist s3://{preview_bucket} --recursive --metadata site-id=ocw-hugo-themes
-                            aws s3{CLI_ENDPOINT_URL} cp {OCW_HUGO_THEMES_GIT_IDENTIFIER}/base-theme/dist s3://{publish_bucket} --recursive --metadata site-id=ocw-hugo-themes
-                            aws s3{CLI_ENDPOINT_URL} cp {OCW_HUGO_THEMES_GIT_IDENTIFIER}/base-theme/static s3://{preview_bucket} --recursive --metadata site-id=ocw-hugo-themes
-                            aws s3{CLI_ENDPOINT_URL} cp {OCW_HUGO_THEMES_GIT_IDENTIFIER}/base-theme/static s3://{publish_bucket} --recursive --metadata site-id=ocw-hugo-themes
-                            aws s3{CLI_ENDPOINT_URL} cp {OCW_HUGO_THEMES_GIT_IDENTIFIER}/base-theme/data/webpack.json s3://{artifacts_bucket}/ocw-hugo-themes/{ocw_hugo_themes_branch}/webpack.json --metadata site-id=ocw-hugo-themes
+                            aws s3{CLI_ENDPOINT_URL} cp ocw-hugo-themes/base-theme/dist s3://{preview_bucket} --recursive --metadata site-id=ocw-hugo-themes
+                            aws s3{CLI_ENDPOINT_URL} cp ocw-hugo-themes/base-theme/dist s3://{publish_bucket} --recursive --metadata site-id=ocw-hugo-themes
+                            aws s3{CLI_ENDPOINT_URL} cp ocw-hugo-themes/base-theme/static s3://{preview_bucket} --recursive --metadata site-id=ocw-hugo-themes
+                            aws s3{CLI_ENDPOINT_URL} cp ocw-hugo-themes/base-theme/static s3://{publish_bucket} --recursive --metadata site-id=ocw-hugo-themes
+                            aws s3{CLI_ENDPOINT_URL} cp ocw-hugo-themes/base-theme/data/webpack.json s3://{artifacts_bucket}/ocw-hugo-themes/{ocw_hugo_themes_branch}/webpack.json --metadata site-id=ocw-hugo-themes
                             """,
                         ],
                     ),
                 ),
             ),
         ]
-        job = Job(name=self.build_theme_assets_job_identifier, serial=True)
+        job = Job(name=self._build_theme_assets_job_identifier, serial=True)
         if not is_dev():
             resource_types.append(slack_notification_resource())
-            resources.append(self.slack_resource)
+            resources.append(self._slack_resource)
             tasks.append(
                 ClearCdnCacheStep(
-                    name=self.clear_draft_cdn_cache_task_identifier,
+                    name=self._clear_draft_cdn_cache_task_identifier,
                     fastly_var="fastly_draft",
                     purge_url="purge/ocw-hugo-themes",
                 )
             )
             tasks.append(
                 ClearCdnCacheStep(
-                    name=self.clear_live_cdn_cache_identifier,
+                    name=self._clear_live_cdn_cache_identifier,
                     fastly_var="fastly_live",
                     purge_url="purge/ocw-hugo-themes",
                 )
