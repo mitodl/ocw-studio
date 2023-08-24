14a91df838040fa7be4a6c4650d86c220dc50309
Carey Gumaer <gumaerc@mit.edu>
2023-08-24 19:43:22 -0400

    upgrade concourse to 7.10, concourse-db to latest postgres, add separate worker container and pregenerate ssh / rsa keys

---
+++ scripts/concourse/keys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ssh-keygen -t rsa -b 4096 -m PEM -f /concourse-keys/session_signing_key -q -N "" <<< $'ny' >/dev/null 2>&1
+ssh-keygen -t rsa -b 4096 -m PEM -f /concourse-keys/tsa_host_key -q -N "" <<< $'ny' >/dev/null 2>&1
+ssh-keygen -t rsa -b 4096 -m PEM -f /concourse-keys/worker_key -q -N "" <<< $'ny' >/dev/null 2>&1
+cp /concourse-keys/worker_key.pub /concourse-keys/authorized_worker_keys
+chmod 600 /concourse-keys/authorized_worker_keys 
