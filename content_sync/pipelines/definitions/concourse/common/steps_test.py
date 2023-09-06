b5704405ab2fca473522cc96cac8cbcc864c8f91
Carey Gumaer <gumaerc@mit.edu>
2023-09-06 11:58:39 -0400

    add test to ensure inputs are blank on reusable PutStep type steps

--- content_sync/pipelines/definitions/concourse/common/steps_test.py
+++ content_sync/pipelines/definitions/concourse/common/steps_test.py
@@ -1,10 +1,12 @@
 """Tests for Concourse Steps"""
+import json
 import pytest
 from ol_concourse.lib.models.pipeline import GetStep, PutStep, Step, TaskStep
 
 from content_sync.pipelines.definitions.concourse.common.steps import (
     ErrorHandlingStep,
     OcwStudioWebhookStep,
+    OpenDiscussionsWebhookStep,
     SlackAlertStep,
     add_error_handling,
 )
@@ -58,3 +60,31 @@ def test_calling_add_error_handling_twice(step_type):
             short_id="test-site",
             instance_vars="?site:test-site",
         )
+
+
+def test_put_steps_empty_inputs():
+    assert (
+        json.loads(
+            OcwStudioWebhookStep(
+                pipeline_name="test_pipeline", status="test_status"
+            ).model_dump_json(by_alias=True)
+        )["try"]["inputs"]
+        == []
+    )
+    assert (
+        json.loads(
+            OpenDiscussionsWebhookStep(
+                pipeline_name="test_pipeline",
+                site_url="http://ocw.mit.edu/courses/test_course",
+            ).model_dump_json(by_alias=True)
+        )["try"]["inputs"]
+        == []
+    )
+    assert (
+        json.loads(
+            SlackAlertStep(alert_type="test_alert", text="test alert").model_dump_json(
+                by_alias=True
+            )
+        )["try"]["do"][0]["inputs"]
+        == []
+    )
