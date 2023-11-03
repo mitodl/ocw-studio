c0499b378b5a76b4ebc4dacc0db6ac2004900a4a
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2023-11-03 15:16:32 +0500

    update pipeline test

--- content_sync/pipelines/definitions/concourse/site_pipeline_test.py
+++ content_sync/pipelines/definitions/concourse/site_pipeline_test.py
@@ -457,7 +457,7 @@ def test_generate_theme_assets_pipeline_definition(  # noqa: C901, PLR0912, PLR0
         filter_webpack_artifacts_task["config"]["run"]["args"]
     )
     assert (
-        f"jq 'recurse | select(type==\"string\")' ./{WEBPACK_MANIFEST_S3_IDENTIFIER}/webpack.json | tr -d '\"' | xargs -I {{}} aws s3{cli_endpoint_url} cp s3://{config.vars['web_bucket']}{{}} ./{WEBPACK_ARTIFACTS_IDENTIFIER}/{{}} --exclude *.js.map"
+        f"jq -r 'values[]' ./{WEBPACK_MANIFEST_S3_IDENTIFIER}/webpack.json | xargs -I {{}} aws s3{cli_endpoint_url} cp s3://{config.vars['web_bucket']}{{}} ./{WEBPACK_ARTIFACTS_IDENTIFIER}/{{}} --exclude *.js.map"
         in filter_webpack_artifacts_command
     )
     if is_dev:
