79edd61f681f645c883bc8022c3994f7c8f79295
pt2302 <1553279+pt2302@users.noreply.github.com>
2024-03-01 13:26:04 -0800

    Copy videos from one course to another (#2120)
    * Copy videos and transcripts from one course to another
    
    * Refactoring copying resources
    
    * Fixing utility function
    
    * Strip whitespace from comma-delimited UUID argument
    
    * Making command idempotent
    
    * Properly set transcript and captions paths
    
    * Properly setting dirpath for new resources

--- videos/utils.py
+++ videos/utils.py
@@ -4,10 +4,14 @@ This module provides several utility functions that simplify the task of working
 """  # noqa: E501
 import os
 import re
+from copy import deepcopy
 
-from main.utils import get_dirpath_and_filename, get_file_extension
+from django.conf import settings
+
+from main.s3_utils import get_boto3_resource
+from main.utils import get_dirpath_and_filename, get_file_extension, uuid_string
 from videos.models import WebsiteContent
-from websites.models import WebsiteStarter
+from websites.models import Website, WebsiteStarter
 
 
 def generate_s3_path(file_or_webcontent, website):
@@ -46,3 +50,63 @@ def get_content_dirpath(slug, collection_type):
             continue
         return collection["folder"]
     return None
+
+
+def copy_obj_s3(source_obj: WebsiteContent, dest_course: Website) -> str:
+    """Copy source_obj to the S3 bucket of dest_course"""
+    s3 = get_boto3_resource("s3")
+    new_s3_path = generate_s3_path(source_obj, dest_course)
+    s3.Object(settings.AWS_STORAGE_BUCKET_NAME, new_s3_path).copy_from(
+        CopySource=f"{settings.AWS_STORAGE_BUCKET_NAME.rstrip('/')}/{str(source_obj.file).lstrip('/')}"
+    )
+    return new_s3_path
+
+
+def create_new_content(source_obj, to_course):
+    """Create new WebsiteContent object from source_obj in to_course,
+    or update existing object if it exists.
+    """
+    new_text_id = uuid_string()
+    if source_obj.file:
+        new_s3_loc = copy_obj_s3(source_obj, to_course)
+        new_dirpath = "content/resources"
+        new_filename = get_dirpath_and_filename(new_s3_loc)[1]
+    else:
+        new_s3_loc = source_obj.file
+        new_dirpath = source_obj.dirpath
+        new_filename = source_obj.filename
+    new_obj_metadata = update_metadata(source_obj, new_text_id, new_s3_loc)
+    existing_content = WebsiteContent.objects.filter(
+        website=to_course, dirpath=new_dirpath, filename=new_filename
+    ).first()
+    if existing_content is None:
+        new_obj = WebsiteContent.objects.create(
+            website=to_course,
+            text_id=new_text_id,
+            metadata=new_obj_metadata,
+            title=source_obj.title,
+            type=source_obj.type,
+            file=new_s3_loc,
+            dirpath=new_dirpath,
+            filename=new_filename,
+            is_page_content=True,
+        )
+        new_obj.save()
+    else:
+        existing_content.metadata = new_obj_metadata
+        existing_content.title = source_obj.title
+        existing_content.type = source_obj.type
+        existing_content.file = new_s3_loc
+        existing_content.dirpath = new_dirpath
+        existing_content.filename = new_filename
+        existing_content.is_page_content = True
+        existing_content.save()
+    return new_obj if existing_content is None else existing_content
+
+
+def update_metadata(source_obj, new_uid, new_s3_path):
+    """Generate updated metadata for new WebsiteContent object"""
+    new_metadata = deepcopy(source_obj.metadata)
+    new_metadata["uid"] = new_uid
+    new_metadata["file"] = str(new_s3_path).lstrip("/")
+    return new_metadata
