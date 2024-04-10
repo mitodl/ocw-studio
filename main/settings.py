e00c6e4109aee5a99e2f3b38821c837ea4883d88
Carey Gumaer <gumaerc@mit.edu>
2024-04-10 16:09:51 -0400

    DEFAULT_FILE_STORAGE is deprecated, set STORAGES instead

--- main/settings.py
+++ main/settings.py
@@ -473,7 +473,7 @@ if OCW_STUDIO_USE_S3 and (
     msg = "You have enabled S3 support, but are missing one of AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, or AWS_STORAGE_BUCKET_NAME"  # noqa: E501
     raise ImproperlyConfigured(msg)
 if OCW_STUDIO_USE_S3:
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    STORAGES = {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
 
 
 # Google Drive and Video settings
