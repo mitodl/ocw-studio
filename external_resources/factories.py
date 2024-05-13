63f0c1ba1a52a3831045f9028aa48659eb8381ec
Umar Hassan <umar.hassan8@gmail.com>
2024-05-13 11:00:21 +0500

    added tests for external resources

---
+++ external_resources/factories.py
@@ -0,0 +1,20 @@
+import factory
+import pytz
+
+from external_resources.models import ExternalResourceState
+from websites.factories import WebsiteContentFactory
+
+
+class ExternalResourceStateFactory(factory.django.DjangoModelFactory):
+    """External Resource Factory"""
+
+    class Meta:  # noqa: D106
+        model = ExternalResourceState
+
+    content = factory.SubFactory(WebsiteContentFactory)
+    status = ExternalResourceState.Status.UNCHECKED
+    external_url_response_code = factory.Faker("random_int", min=100, max=599)
+    backup_url_response_code = factory.Faker("random_int", min=100, max=599)
+    is_external_url_broken = factory.Faker("boolean")
+    is_backup_url_broken = factory.Faker("boolean")
+    last_checked = factory.Faker("date_time", tzinfo=pytz.utc)
