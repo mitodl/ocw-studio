bb3b252f9497e46cd023a5898befde16053ac4b0
Umar Hassan <umar.hassan8@gmail.com>
2024-06-05 04:03:51 +0500

    refactored hard coded status codes in api test

--- external_resources/api_test.py
+++ external_resources/api_test.py
@@ -1,6 +1,7 @@
 """Tests for External Resources API"""
 
 import pytest
+from rest_framework.status import HTTP_200_OK
 
 from external_resources.api import is_url_broken
 from external_resources.constants import RESOURCE_UNCHECKED_STATUSES
@@ -9,12 +10,12 @@ from external_resources.exceptions import CheckFailedError
 
 def test_is_url_broken_valid(mocker):
     """Test for working url"""
-    mock_response = mocker.Mock(status_code=200)
+    mock_response = mocker.Mock(status_code=HTTP_200_OK)
     mocker.patch("external_resources.api.requests.head", return_value=mock_response)
 
     result, status_code = is_url_broken("http://google.com")
     assert not result
-    assert status_code == 200
+    assert status_code == HTTP_200_OK
 
 
 @pytest.mark.parametrize("status_code", RESOURCE_UNCHECKED_STATUSES)
