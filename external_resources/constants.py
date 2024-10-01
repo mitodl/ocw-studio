19ace211f03403c7f91a35f84815756d78dbea17
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2024-10-01 23:09:18 +0500

    Use constants for wayback machine functionalities

--- external_resources/constants.py
+++ external_resources/constants.py
@@ -25,6 +25,8 @@ RESOURCE_UNCHECKED_STATUSES = [
 # constants for Celery task
 EXTERNAL_RESOURCE_TASK_RATE_LIMIT = "100/s"
 EXTERNAL_RESOURCE_TASK_PRIORITY = 4  # Lowest priority from range (0 - 4)
+WAYBACK_MACHINE_TASK_RATE_LIMIT = "0.1/s"
+WAYBACK_MACHINE_TASK_PRIORITY = 4
 
 # constants for user agent
 USER_AGENT_STRING = (
@@ -41,7 +43,7 @@ METADATA_URL_STATUS_CODE = "url_status_code"
 
 # constants for Wayback Machine integration
 WAYBACK_API_URL = "https://web.archive.org/save"
-WAYBACK_CHECK_STATUS_URL_TEMPLATE = "https://web.archive.org/save/status/{job_id}"
+WAYBACK_CHECK_STATUS_URL = "https://web.archive.org/save/status"
 WAYBACK_PENDING_STATUS = "pending"
 WAYBACK_SUCCESS_STATUS = "success"
 WAYBACK_ERROR_STATUS = "error"
