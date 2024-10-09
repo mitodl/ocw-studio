a34157b5e1553d08b8ee4df4e157225b07385b19
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2024-10-09 18:40:49 +0500

    Add management command for submission of sites

---
+++ external_resources/management/commands/submit_sites_to_wayback.py
@@ -0,0 +1,34 @@
+"""Management command to submit external resources of websites to the Wayback Machine"""
+
+from external_resources.tasks import submit_website_resources_to_wayback_task
+from main.management.commands.filter import WebsiteFilterCommand
+from websites.models import Website
+
+
+class Command(WebsiteFilterCommand):
+    """Submit external resources of websites to the Wayback Machine"""
+
+    help = __doc__
+
+    def handle(self, *args, **options):
+        super().handle(*args, **options)
+        force_submission = options.get("force", False)
+
+        websites = Website.objects.all()
+        websites = self.filter_websites(websites)
+
+        if not websites.exists():
+            self.stdout.write("No websites found with the given filters.")
+            return
+
+        self.stdout.write(
+            f"Submitting external resources for {websites.count()} websites."
+        )
+
+        for website in websites:
+            submit_website_resources_to_wayback_task.delay(
+                website.name, ignore_last_submission=force_submission
+            )
+            self.stdout.write(f"Enqueued submission for website: {website.name}")
+
+        self.stdout.write("All tasks have been enqueued.")
