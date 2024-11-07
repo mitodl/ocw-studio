e8b8af8e5bf265ad1eef01e0aa67663719476b93
Umar Hassan <umar.hassan8@gmail.com>
2024-11-08 02:44:45 +0500

    added posthog disable for testing

--- main/posthog.py
+++ main/posthog.py
@@ -1,8 +1,16 @@
 from posthog import Posthog
 
-from main.settings import ENVIRONMENT, POSTHOG_API_HOST, POSTHOG_PROJECT_API_KEY
+from main.settings import (
+    ENVIRONMENT,
+    POSTHOG_API_HOST,
+    POSTHOG_ENABLED,
+    POSTHOG_PROJECT_API_KEY,
+)
 
 posthog = Posthog(POSTHOG_PROJECT_API_KEY, host=POSTHOG_API_HOST)
+
+if not POSTHOG_ENABLED:
+    posthog.disabled = True
 
 if ENVIRONMENT == "dev":
     posthog.debug = True
