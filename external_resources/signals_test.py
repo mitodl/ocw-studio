0c3f228994ef1802d43181719c80408667c16ed1
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2024-12-05 16:03:31 +0500

    Update signals_test.py

--- external_resources/signals_test.py
+++ external_resources/signals_test.py
@@ -10,11 +10,12 @@ from websites.factories import WebsiteContentFactory
 @pytest.mark.django_db()
 def test_upsert_external_resource_state(mocker):
     """Test that the upsert_external_resource_state signal makes the correct call"""
-    mocker.patch("external_resources.signals.is_feature_enabled", return_value=True)
-
     mock_update_or_create = mocker.patch(
         "external_resources.signals.ExternalResourceState.objects.update_or_create",
         autospec=True,
+    )
+    mock_submit_task = mocker.patch(
+        "external_resources.signals.submit_url_to_wayback_task.delay"
     )
     content = WebsiteContentFactory.create(type=CONTENT_TYPE_EXTERNAL_RESOURCE)
     mock_update_or_create.assert_called_once_with(
@@ -31,3 +32,4 @@ def test_upsert_external_resource_state(mocker):
             "wayback_last_successful_submission": None,
         },
     )
+    mock_submit_task.assert_called_once_with(content.id)
