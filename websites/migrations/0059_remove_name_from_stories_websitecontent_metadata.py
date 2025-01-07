18abf1af60694fb86f06c39e3bf751d6fe48f26c
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2025-01-07 18:05:00 +0500

    Add a rollback to migration

--- websites/migrations/0059_remove_name_from_stories_websitecontent_metadata.py
+++ websites/migrations/0059_remove_name_from_stories_websitecontent_metadata.py
@@ -1,10 +1,17 @@
 # Manual migration to remove the "name" field from the metadata of existing stories
 
+import logging
 
 from django.db import migrations
 
+logger = logging.getLogger(__name__)
 
-def remove_name_from_stories_metadata(apps, schema_editor):
+
+def migrate_metadata_forward(apps, schema_editor):
+    """
+    Forward migration remove the "name" field from the metadata
+    of existing stories
+    """
     WebsiteContent = apps.get_model("websites", "WebsiteContent")
     stories = WebsiteContent.objects.filter(website__name="ocw-www", type="stories")
 
@@ -14,11 +21,22 @@ def remove_name_from_stories_metadata(apps, schema_editor):
             story.save(update_fields=["metadata"])
 
 
+def migrate_metadata_backward(apps, schema_editor):
+    """
+    Run migration in backward direction.
+    This will not return the DB to original state.
+    """
+    logger.warning(
+        "The reverse migration for '0059' of the websites"
+        "module cannot restore the original data state."
+    )
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("websites", "0058_websitecontent_referencing_content"),
     ]
 
     operations = [
-        migrations.RunPython(remove_name_from_stories_metadata),
+        migrations.RunPython(migrate_metadata_forward, migrate_metadata_backward),
     ]
