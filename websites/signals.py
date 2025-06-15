9efd7d8eac3173bdadc660772b8dfd7315f8585e
pt2302 <1553279+pt2302@users.noreply.github.com>
2025-06-15 19:43:10 -0400

    Update page URL when title changes

--- websites/signals.py
+++ websites/signals.py
@@ -1,10 +1,11 @@
 """Signals for websites"""
 
 from django.db import transaction
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from django.utils.text import slugify
 
-from websites.models import Website
+from websites.models import Website, WebsiteContent
 from websites.permissions import setup_website_groups_permissions
 
 
@@ -25,3 +26,20 @@ def handle_website_save(
     """
     if created:
         setup_website_groups_permissions(instance)
+
+
+@receiver(pre_save, sender=WebsiteContent)
+def update_page_url_on_title_change(_sender, instance, **_kwargs):
+    """Update page URL when title changes for page content"""
+
+    if instance.is_page_content and instance.title:
+        new_filename = slugify(instance.title)
+        cur_filename = WebsiteContent.objects.filter(
+            website=instance.website,
+            dirpath=instance.dirpath,
+            filename=new_filename,
+            is_page_content=True,
+        ).exclude(pk=instance.pk)
+
+        if not cur_filename.exists():
+            instance.filename = new_filename
