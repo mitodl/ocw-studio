cb061b578f06bf01d1adf8ea308e631389cbcf27
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2025-07-28 19:04:55 +0500

    Update metadata field

--- videos/tasks.py
+++ videos/tasks.py
@@ -583,4 +583,4 @@ def populate_video_file_size(video_content_id: int):
     if response.status_code == HTTP_200_OK and "Content-Length" in response.headers:
         video.metadata["file_size"] = int(response.headers["Content-Length"])
         video.skip_sync = True
-        video.save()
+        video.save(update_fields=["metadata"])
