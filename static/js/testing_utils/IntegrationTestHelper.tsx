39c380b39dc97c8b693f46063330b9acac5f4d58
pt2302 <1553279+pt2302@users.noreply.github.com>
2025-12-10 20:28:53 -0500

    Wrap IntegrationTestHelper callbacks

--- static/js/testing_utils/IntegrationTestHelper.tsx
+++ static/js/testing_utils/IntegrationTestHelper.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { cloneDeep, defaultsDeep } from "lodash"
 import { render, RenderOptions } from "@testing-library/react"
+import { act } from "@testing-library/react"
 import { Provider } from "react-redux"
 import { Provider as ReduxQueryProvider } from "redux-query-react"
 import { createMemoryHistory, InitialEntry } from "history"
@@ -78,15 +79,20 @@ export default class IntegrationTestHelper {
     const mockMakeRequest = jest.spyOn(networkInterfaceFuncs, "makeRequest")
     mockMakeRequest.mockClear()
     mockMakeRequest.mockImplementation((url, method, options) => ({
-      execute: async (callback) => {
-        const response = await this.handleRequest(url, method, options)
-        const err = null
-        const resStatus = response.status ?? 0
-        const resBody = response.body ?? undefined
-        const resText = response.text ?? undefined
-        const resHeaders = response.headers ?? undefined
+      execute: (callback) => {
+        const executeAsync = async () => {
+          const response = await this.handleRequest(url, method, options)
+          const err = null
+          const resStatus = response.status ?? 0
+          const resBody = response.body ?? undefined
+          const resText = response.text ?? undefined
+          const resHeaders = response.headers ?? undefined
 
-        callback(err, resStatus, resBody, resText, resHeaders)
+          act(() => {
+            callback(err, resStatus, resBody, resText, resHeaders)
+          })
+        }
+        executeAsync()
       },
       abort: () => {
         throw new Error("Aborts currently unhandled")
