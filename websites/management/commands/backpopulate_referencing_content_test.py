90b414ac3f5248d2aca81e9dec1153401a3b196e
Umar Hassan <umar.hassan8@gmail.com>
2026-02-11 18:58:19 +0500

    feat: added tracking with referencing_content for course home image, video gallery items, resource list items, and other relation-backed content.

--- websites/management/commands/backpopulate_referencing_content_test.py
+++ websites/management/commands/backpopulate_referencing_content_test.py
@@ -8,10 +8,15 @@ from django.core.management import call_command
 from django.test import TestCase
 
 from websites.constants import (
+    CONTENT_TYPE_COURSE_COLLECTION,
     CONTENT_TYPE_INSTRUCTOR,
     CONTENT_TYPE_METADATA,
     CONTENT_TYPE_PAGE,
+    CONTENT_TYPE_PROMO,
     CONTENT_TYPE_RESOURCE,
+    CONTENT_TYPE_RESOURCE_LIST,
+    CONTENT_TYPE_TESTIMONIAL,
+    CONTENT_TYPE_VIDEO_GALLERY,
 )
 from websites.factories import WebsiteContentFactory, WebsiteFactory
 from websites.management.commands.backpopulate_referencing_content import Command
@@ -336,3 +341,417 @@ def test_instructor_references_with_website_filter(
     assert len(referenced_content) == 2
     for instructor in instructors:
         assert instructor in referenced_content
+
+
+@pytest.mark.django_db
+def test_course_image_references_detected():
+    """Test that course image fields in sitemetadata are detected as references."""
+    website = WebsiteFactory.create()
+    image_resource = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    image_thumbnail_resource = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    sitemetadata = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_METADATA,
+        metadata={
+            "course_description": "No references here",
+            "course_image": {
+                "content": image_resource.text_id,
+                "website": website.name,
+            },
+            "course_image_thumbnail": {
+                "content": image_thumbnail_resource.text_id,
+                "website": website.name,
+            },
+        },
+    )
+
+    assert sitemetadata.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    sitemetadata.refresh_from_db()
+    referenced_content = list(sitemetadata.referenced_by.all())
+    assert len(referenced_content) == 2
+    assert image_resource in referenced_content
+    assert image_thumbnail_resource in referenced_content
+
+
+@pytest.mark.django_db
+def test_video_gallery_references_detected():
+    """Test that video UUIDs in video_gallery metadata are detected as references"""
+    website = WebsiteFactory.create()
+    video1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    video2 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    video_gallery = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_VIDEO_GALLERY,
+        metadata={
+            "videos": {
+                "content": [video1.text_id, video2.text_id],
+                "website": website.name,
+            },
+        },
+    )
+
+    assert video_gallery.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    video_gallery.refresh_from_db()
+    referenced_content = list(video_gallery.referenced_by.all())
+    assert len(referenced_content) == 2
+    assert video1 in referenced_content
+    assert video2 in referenced_content
+
+
+@pytest.mark.django_db
+def test_resource_list_resources_references_detected():
+    """Test that resource UUIDs in resource_list metadata are detected as references"""
+    website = WebsiteFactory.create()
+    resource1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    resource2 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    resource_list = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE_LIST,
+        metadata={
+            "description": "No references here",
+            "resources": {
+                "content": [resource1.text_id, resource2.text_id],
+                "website": website.name,
+            },
+        },
+    )
+
+    assert resource_list.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    resource_list.refresh_from_db()
+    referenced_content = list(resource_list.referenced_by.all())
+    assert len(referenced_content) == 2
+    assert resource1 in referenced_content
+    assert resource2 in referenced_content
+
+
+@pytest.mark.django_db
+def test_page_with_embedded_href_uuid_references_detected():
+    """Test that href_uuid in embedded resources are detected as references"""
+    website = WebsiteFactory.create()
+    resource1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    resource2 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    page = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_PAGE,
+        markdown=(
+            f'{{{{< resource uuid="{resource1.text_id}" '
+            f'href_uuid="{resource2.text_id}" >}}}}'
+        ),
+    )
+
+    assert page.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    page.refresh_from_db()
+    referenced_content = list(page.referenced_by.all())
+    assert len(referenced_content) == 2
+    assert resource1 in referenced_content
+    assert resource2 in referenced_content
+
+
+@pytest.mark.django_db
+def test_course_collection_references_detected():
+    """Test that course-collection cover-image and courselists are detected"""
+    website = WebsiteFactory.create()
+    resource1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    course_list1 = WebsiteContentFactory.create(
+        website=website,
+        type="course-lists",
+    )
+    course_list2 = WebsiteContentFactory.create(
+        website=website,
+        type="course-lists",
+    )
+    course_collection = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_COURSE_COLLECTION,
+        metadata={
+            "title": "My Collection",
+            "cover-image": {"content": resource1.text_id},
+            "courselists": {"content": [course_list1.text_id, course_list2.text_id]},
+        },
+    )
+
+    assert course_collection.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    course_collection.refresh_from_db()
+    referenced_content = list(course_collection.referenced_by.all())
+    assert len(referenced_content) == 3
+    assert resource1 in referenced_content
+    assert course_list1 in referenced_content
+    assert course_list2 in referenced_content
+
+
+@pytest.mark.django_db
+def test_promo_references_detected():
+    """Test that promo image field is detected as a reference"""
+    website = WebsiteFactory.create()
+    resource1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    promo = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_PROMO,
+        metadata={
+            "title": "Special Promo",
+            "image": {"content": resource1.text_id},
+        },
+    )
+
+    assert promo.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    promo.refresh_from_db()
+    referenced_content = list(promo.referenced_by.all())
+    assert len(referenced_content) == 1
+    assert resource1 in referenced_content
+
+
+@pytest.mark.django_db
+def test_testimonial_references_detected():
+    """Test that testimonial image field is detected as a reference"""
+    website = WebsiteFactory.create()
+    resource1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    testimonial = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_TESTIMONIAL,
+        metadata={
+            "title": "John Doe",
+            "image": {"content": resource1.text_id},
+        },
+    )
+
+    assert testimonial.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    testimonial.refresh_from_db()
+    referenced_content = list(testimonial.referenced_by.all())
+    assert len(referenced_content) == 1
+    assert resource1 in referenced_content
+
+
+@pytest.mark.django_db
+def test_testimonial_with_markdown_references():
+    """Test testimonial with both image field and markdown references"""
+    website = WebsiteFactory.create()
+    resource1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    resource2 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    testimonial = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_TESTIMONIAL,
+        markdown=f"Testimonial text {{{{< resource {resource2.text_id} >}}}}",
+        metadata={
+            "title": "Jane Smith",
+            "image": {"content": resource1.text_id},
+        },
+    )
+
+    assert testimonial.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    testimonial.refresh_from_db()
+    referenced_content = list(testimonial.referenced_by.all())
+    assert len(referenced_content) == 2
+    assert resource1 in referenced_content
+    assert resource2 in referenced_content
+
+
+@pytest.mark.django_db
+def test_course_collection_with_description_markdown():
+    """Test course-collection with description field containing resource links"""
+    website = WebsiteFactory.create()
+    resource1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    resource2 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    course_list = WebsiteContentFactory.create(
+        website=website,
+        type="course-lists",
+    )
+    course_collection = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_COURSE_COLLECTION,
+        markdown=f"Intro {{{{< resource {resource1.text_id} >}}}}",
+        metadata={
+            "title": "Collection with Description",
+            "description": f"Description {{{{< resource {resource2.text_id} >}}}}",
+            "courselists": {"content": [course_list.text_id]},
+        },
+    )
+
+    assert course_collection.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    course_collection.refresh_from_db()
+    referenced_content = list(course_collection.referenced_by.all())
+    assert len(referenced_content) == 3
+    assert resource1 in referenced_content
+    assert resource2 in referenced_content
+    assert course_list in referenced_content
+
+
+@pytest.mark.django_db
+def test_promo_with_markdown_references():
+    """Test promo with both image field and markdown references"""
+    website = WebsiteFactory.create()
+    resource1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    resource2 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    promo = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_PROMO,
+        markdown=f"Promo content {{{{< resource {resource2.text_id} >}}}}",
+        metadata={
+            "title": "Special Promo",
+            "image": {"content": resource1.text_id},
+        },
+    )
+
+    assert promo.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    promo.refresh_from_db()
+    referenced_content = list(promo.referenced_by.all())
+    assert len(referenced_content) == 2
+    assert resource1 in referenced_content
+    assert resource2 in referenced_content
+
+
+@pytest.mark.django_db
+def test_course_collection_empty_courselists():
+    """Test course-collection with empty courselists still populates cover-image"""
+    website = WebsiteFactory.create()
+    resource1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    course_collection = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_COURSE_COLLECTION,
+        metadata={
+            "title": "Empty Collection",
+            "cover-image": {"content": resource1.text_id},
+            "courselists": {"content": []},
+        },
+    )
+
+    assert course_collection.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    course_collection.refresh_from_db()
+    referenced_content = list(course_collection.referenced_by.all())
+    assert len(referenced_content) == 1
+    assert resource1 in referenced_content
+
+
+@pytest.mark.django_db
+def test_testimonial_no_image_only_markdown():
+    """Test testimonial with only markdown references, no image field"""
+    website = WebsiteFactory.create()
+    resource1 = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_RESOURCE,
+    )
+    testimonial = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_TESTIMONIAL,
+        markdown=f"{{{{< resource {resource1.text_id} >}}}}",
+        metadata={
+            "title": "Bob Johnson",
+        },
+    )
+
+    assert testimonial.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    testimonial.refresh_from_db()
+    referenced_content = list(testimonial.referenced_by.all())
+    assert len(referenced_content) == 1
+    assert resource1 in referenced_content
+
+
+@pytest.mark.django_db
+def test_promo_no_image_field():
+    """Test promo with no image field doesn't create references"""
+    website = WebsiteFactory.create()
+    promo = WebsiteContentFactory.create(
+        website=website,
+        type=CONTENT_TYPE_PROMO,
+        metadata={
+            "title": "Promo without Image",
+            "description": "Just text",
+        },
+    )
+
+    assert promo.referenced_by.count() == 0
+
+    call_command("backpopulate_referencing_content", verbosity=0, stdout=StringIO())
+
+    promo.refresh_from_db()
+    # Should still be 0 since there are no references
+    assert promo.referenced_by.count() == 0
