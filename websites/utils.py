34a10a1ed9b8c7c00df96d8ab93122687d11b07b
Umar Hassan <umar.hassan8@gmail.com>
2026-03-31 23:01:53 +0500

    feat: handled refrencing_content for captions/transcripts and added tests for navmenu objects

--- websites/utils.py
+++ websites/utils.py
@@ -145,6 +145,8 @@ def get_metadata_content_key(content) -> list:
             content_keys = [
                 constants.METADATA_FIELD_IMAGE_CAPTION,
                 constants.METADATA_FIELD_IMAGE_CREDIT,
+                settings.YT_FIELD_CAPTIONS_RESOURCE + ".content",
+                settings.YT_FIELD_TRANSCRIPT_RESOURCE + ".content",
             ]
         case constants.CONTENT_TYPE_COURSE_COLLECTION:
             content_keys = [
