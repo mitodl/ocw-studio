07fb4b51d6653e3c6001e32c17457b012c72056e
pt2302 <1553279+pt2302@users.noreply.github.com>
2026-04-29 18:38:46 -0400

    Flag updated websites as needing republishing

--- websites/migrations/0072_remove_lecture_videos_lrt_from_non_video_resources.py
+++ websites/migrations/0072_remove_lecture_videos_lrt_from_non_video_resources.py
@@ -13,18 +13,25 @@ LECTURE_VIDEOS_LRT = "Lecture Videos"
 def remove_lecture_videos_lrt_from_non_video_resources(apps, schema_editor):
     """Remove 'Lecture Videos' from learning_resource_types for non-video resources."""
     WebsiteContent = apps.get_model("websites", "WebsiteContent")
+    Website = apps.get_model("websites", "Website")
 
-    resources = WebsiteContent.objects.filter(
-        type=CONTENT_TYPE_RESOURCE,
-        metadata__learning_resource_types__contains=LECTURE_VIDEOS_LRT,
-    ).exclude(metadata__resourcetype=RESOURCE_TYPE_VIDEO)
+    resources = list(
+        WebsiteContent.objects.filter(
+            type=CONTENT_TYPE_RESOURCE,
+            metadata__learning_resource_types__contains=LECTURE_VIDEOS_LRT,
+        ).exclude(metadata__resourcetype=RESOURCE_TYPE_VIDEO)
+    )
 
-    for resource in resources.iterator():
+    for resource in resources:
         resource.metadata["learning_resource_types"] = [
             v for v in resource.metadata["learning_resource_types"]
             if v != LECTURE_VIDEOS_LRT
         ]
-        resource.save(update_fields=["metadata"])
+
+    WebsiteContent.objects.bulk_update(resources, ["metadata"])
+    Website.objects.filter(
+        uuid__in={r.website_id for r in resources}
+    ).update(has_unpublished_live=True, has_unpublished_draft=True)
 
 
 class Migration(migrations.Migration):
